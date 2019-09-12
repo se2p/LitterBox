091e04f5d463b7e5abcb3285f4912d74820a7a7f
Nina <nina.nicole@gmx.de>
2019-09-12 16:21:30 +0200

    Add Scratch 2 opcodes to the opcode mapping. Ref #12
    Only the equivalents to the Scratch 3 opcodes were added.
    If there are blocks which were in Scratch 2 but not in Scratch 3, these are still missing.

--- src/main/java/scratch/data/OpCodeMapping.java
+++ src/main/java/scratch/data/OpCodeMapping.java
@@ -13,140 +13,297 @@ import static scratch.data.OpCodeMapping.BlockShape.HAT;
 import static scratch.data.OpCodeMapping.BlockShape.REPORTER;
 import static scratch.data.OpCodeMapping.BlockShape.STACK;
 
+/**
+ * The OpCodeMapping Class contains lists of all block opcodes of Scratch 2 and 3
+ * sorted by their Block Shape and maps every block opcode to its
+ * corresponding Block Shape.
+ */
 public class OpCodeMapping {
 
+    /**
+     * List of all opcodes of blocks which are Hat Blocks.
+     * Scratch 3 opcodes are followed by their equivalent in Scratch 2.
+     */
     public static final List<String> hatBlockOpcodes = new ArrayList<>(Arrays.asList(
             "event_whenflagclicked",
+            "whenGreenFlag",
             "event_whenkeypressed",
+            "whenKeyPressed",
             "event_whenthisspriteclicked",
+            "whenClicked",
             "event_whenbroadcastreceived",
+            "whenIReceive",
             "event_whenbackdropswitchesto",
+            "whenSceneStarts",
             "control_start_as_clone",
+            "whenCloned",
             "event_whengreaterthan",
+            "whenSensorGreaterThan",
             "procedures_definition"
-    ));
-    public static final List<String> stackBlockOpcodes = new ArrayList<>(Arrays.asList(
-            "motion_movesteps",
-            "motion_turnright",
-            "motion_turnleft",
-            "motion_goto",
-            "motion_gotoxy",
-            "motion_glideto",
-            "motion_glidesecstoxy",
-            "motion_pointindirection",
-            "motion_pointtowards",
-            "motion_changexby",
-            "motion_setx",
-            "motion_changeyby",
-            "motion_sety",
-            "motion_ifonedgebounce",
-            "motion_setrotationstyle",
-            "looks_sayforsecs",
-            "looks_say",
-            "looks_thinkforsecs",
-            "looks_think",
-            "looks_switchcostumeto",
-            "looks_switchbackdropto",
-            "looks_nextcostume",
-            "looks_nextbackdrop",
-            "looks_changesizeby",
-            "looks_setsizeto",
-            "looks_changeeffectby",
-            "looks_seteffectto",
-            "looks_cleargraphiceffects",
-            "looks_show",
-            "looks_hide",
-            "looks_gotofrontback",
-            "looks_goforwardbackwardlayers",
-            "sound_playuntildone",
-            "sound_play",
-            "sound_stopallsounds",
-            "sound_changeeffectby",
-            "sound_seteffectto",
-            "sound_cleareffects",
-            "sound_changevolumeby",
-            "sound_setvolumeto",
-            "event_broadcast",
-            "event_broadcastandwait",
-            "control_wait",
-            "control_wait_until",
-            "control_create_clone_of",
-            "sensing_askandwait",
-            "sensing_setdragmode",
-            "sensing_resettimer",
-            "data_setvariableto",
-            "data_changevariableby",
-            "data_showvariable",
-            "data_hidevariable",
-            "data_addtolist",
-            "data_deleteoflist",
-            "data_deletealloflist",
-            "data_insertatlist",
-            "data_replaceitemoflist",
-            "data_showlist",
-            "data_hidelist",
-            "procedures_call"
-    ));
-    public static final List<String> booleanBlockOpcodes = new ArrayList<>(Arrays.asList(
-            "sensing_touchingobject",
-            "sensing_touchingcolor",
-            "sensing_coloristouchingcolor",
-            "sensing_keypressed",
-            "sensing_mousedown",
-            "operator_gt",
-            "operator_lt",
-            "operator_equals",
-            "operator_and",
-            "operator_or",
-            "operator_not",
-            "operator_contains",
-            "data_listcontainsitem"
-    ));
-    public static final List<String> reporterBlockOpcodes = new ArrayList<>(Arrays.asList(
-            "motion_xposition",
-            "motion_yposition",
-            "motion_direction",
-            "looks_size",
-            "looks_costumenumbername",
-            "looks_backdropnumbername",
-            "sound_volume",
-            "sensing_distanceto",
-            "sensing_answer",
-            "sensing_mousex",
-            "sensing_mousey",
-            "sensing_loudness",
-            "sensing_timer",
-            "sensing_of",
-            "sensing_current",
-            "sensing_dayssince2000",
-            "sensing_username",
-            "operator_add",
-            "operator_subtract",
-            "operator_multiply",
-            "operator_divide",
-            "operator_random",
-            "operator_join",
-            "operator_letter_of",
-            "operator_length",
-            "operator_mod",
-            "operator_round",
-            "operator_mathop",
-            "data_itemoflist",
-            "data_itemnumoflist",
-            "data_lengthoflist"
-    ));
-    public static final List<String> cBlockOpcodes = new ArrayList<>(Arrays.asList(
-            "control_repeat",
-            "control_forever",
-            "control_if",
-            "control_if_else",
-            "control_repeat_until"
-    ));
-    public static final List<String> capBlockOpcodes = new ArrayList<>(Arrays.asList(
-            "control_stop",
-            "control_delete_this_clone"
+            // TODO find Scratch 2 opcode of custom block definition
     ));
 
+    /**
+     * List of all opcodes of blocks which are Stack Blocks.
+     * Scratch 3 opcodes are followed by their equivalent in Scratch 2.
+     */
+    public static final List<String> stackBlockOpcodes = new ArrayList<>(Arrays.asList(
+            "motion_movesteps",
+            "forward:",
+            "motion_turnright",
+            "turnRight:",
+            "motion_turnleft",
+            "turnLeft:",
+            "motion_goto",
+            "gotoSpriteOrMouse:",
+            "motion_gotoxy",
+            "gotoX:y:",
+            "motion_glideto",
+            // There is no Glide Secs to block in Scratch 2.
+            "motion_glidesecstoxy",
+            "glideSecs:toX:y:elapsed:from:",
+            "motion_pointindirection",
+            "heading:",
+            "motion_pointtowards",
+            "pointTowards:",
+            "motion_changexby",
+            "changeXposBy:",
+            "motion_setx",
+            "xpos:",
+            "motion_changeyby",
+            "changeYposBy:",
+            "motion_sety",
+            "ypos:",
+            "motion_ifonedgebounce",
+            "bounceOffEdge",
+            "motion_setrotationstyle",
+            "setRotationStyle",
+            "looks_sayforsecs",
+            "say:duration:elapsed:from:",
+            "looks_say",
+            "say:",
+            "looks_thinkforsecs",
+            "hink:duration:elapsed:from:",
+            "looks_think",
+            "think:",
+            "looks_switchcostumeto",
+            "lookLike:",
+            "looks_switchbackdropto",
+            "startScene",
+            "looks_nextcostume",
+            "nextCostume",
+            "looks_nextbackdrop",
+            "nextScene",
+            "looks_changesizeby",
+            "changeSizeBy:",
+            "looks_setsizeto",
+            "setSizeTo:",
+            "looks_changeeffectby",
+            "changeGraphicEffect:by:",
+            "looks_seteffectto",
+            "setGraphicEffect:to:",
+            "looks_cleargraphiceffects",
+            "filterReset",
+            "looks_show",
+            "show",
+            "looks_hide",
+            "hide",
+            "looks_gotofrontback",
+            "comeToFront",
+            "looks_goforwardbackwardlayers",
+            "sound_playuntildone",
+            "doPlaySoundAndWait",
+            "sound_play",
+            "playSound:",
+            "sound_stopallsounds",
+            "stopAllSounds",
+            "sound_changeeffectby",
+            "changeGraphicEffect:by:",
+            "sound_seteffectto",
+            "setGraphicEffect:to:",
+            "sound_cleareffects",
+            // There is no Clear Sound Effects block in Scratch 2.
+            "sound_changevolumeby",
+            "changeVolumeBy:",
+            "sound_setvolumeto",
+            "setVolumeTo:",
+            "event_broadcast",
+            "broadcast:",
+            "event_broadcastandwait",
+            "doBroadcastAndWait",
+            "control_wait",
+            "wait:elapsed:from:",
+            "control_wait_until",
+            "doWaitUntil",
+            "control_create_clone_of",
+            "createCloneOf",
+            "sensing_askandwait",
+            "doAsk",
+            "sensing_setdragmode",
+            // There is no Set Drag Mode block in Scratch 2.
+            "sensing_resettimer",
+            "timerReset",
+            "data_setvariableto",
+            "setVar:to:",
+            "data_changevariableby",
+            "changeVar:by:",
+            "data_showvariable",
+            "showVariable:",
+            "data_hidevariable",
+            "hideVariable:",
+            "data_addtolist",
+            "append:toList:",
+            "data_deleteoflist",
+            "deleteLine:ofList:",
+            "data_deletealloflist",
+            // There is no Delete All of block in Scratch 2.
+            "data_insertatlist",
+            "setLine:ofList:to:",
+            "data_replaceitemoflist",
+            "insert:at:ofList:",
+            "data_showlist",
+            "showList:",
+            "data_hidelist",
+            "hideList:",
+            "procedures_call"
+            // TODO find Scratch 2 opcode of custom block call
+    ));
+
+    /**
+     * List of all opcodes of blocks which are Boolean Blocks.
+     * Scratch 3 opcodes are followed by their equivalent in Scratch 2.
+     */
+    public static final List<String> booleanBlockOpcodes = new ArrayList<>(Arrays.asList(
+            "sensing_touchingobject",
+            "touching:",
+            "sensing_touchingcolor",
+            "touchingColor:",
+            "sensing_coloristouchingcolor",
+            "color:sees:",
+            "sensing_keypressed",
+            "keyPressed:",
+            "sensing_mousedown",
+            "mousePressed",
+            "operator_gt",
+            ">",
+            "operator_lt",
+            "<",
+            "operator_equals",
+            "=",
+            "operator_and",
+            "&",
+            "operator_or",
+            "|",
+            "operator_not",
+            "not",
+            "operator_contains",
+            "list:contains:",
+            "data_listcontainsitem",
+            "list:contains:"
+    ));
+
+    /**
+     * List of all opcodes of blocks which are Reporter Blocks.
+     * Scratch 3 opcodes are followed by their equival'ent in Scratch 2.
+     * Variables and Lists are Reporter Blocks, too, but do not have an opcode.
+     */
+    public static final List<String> reporterBlockOpcodes = new ArrayList<>(Arrays.asList(
+            "motion_xposition",
+            "xpos",
+            "motion_yposition",
+            "ypos",
+            "motion_direction",
+            "heading",
+            "looks_size",
+            "scale",
+            "looks_costumenumbername",
+            "costumeIndex",
+            "looks_backdropnumbername",
+            "backgroundIndex ",
+            "sound_volume",
+            "volume",
+            "sensing_distanceto",
+            "distanceTo: ",
+            "sensing_answer",
+            "answer",
+            "sensing_mousex",
+            "mouseX",
+            "sensing_mousey",
+            "mouseY",
+            "sensing_loudness",
+            "soundLevel",
+            "sensing_timer",
+            "timer",
+            "sensing_of",
+            "getAttribute:of:",
+            "sensing_current",
+            "timeAndDate ",
+            "sensing_dayssince2000",
+            "timestamp",
+            "sensing_username",
+            "getUserName",
+            "operator_add",
+            "+",
+            "operator_subtract",
+            "-",
+            "operator_multiply",
+            "*",
+            "operator_divide",
+            "/",
+            "operator_random",
+            "randomFrom:to:",
+            "operator_join",
+            "concatenate:with:",
+            "operator_letter_of",
+            "letter:of: ",
+            "operator_length",
+            "stringLength: ",
+            "operator_mod",
+            "%",
+            "operator_round",
+            "round",
+            "operator_mathop",
+            "abs",
+            "data_itemoflist",
+            // There is no item Number of in block in Scratch 2.
+            "data_itemnumoflist",
+            "getLine:ofList:",
+            "data_lengthoflist",
+            "lineCountOfList:"
+    ));
+
+    /**
+     * List of all opcodes of blocks which are C Blocks.
+     * Scratch 3 opcodes are followed by their equivalent in Scratch 2.
+     */
+    public static final List<String> cBlockOpcodes = new ArrayList<>(Arrays.asList(
+            "control_repeat",
+            "doRepeat",
+            "control_forever",
+            "doForever",
+            "control_if",
+            "doIf",
+            "control_if_else",
+            "doIfElse",
+            "control_repeat_until",
+            "doUntil"
+    ));
+
+    /**
+     * List of all opcodes of blocks which are Cap Blocks.
+     * Scratch 3 opcodes are followed by their equivalent in Scratch 2.
+     */
+    public static final List<String> capBlockOpcodes = new ArrayList<>(Arrays.asList(
+            "control_stop",
+            "stopAll",
+            "control_delete_this_clone",
+            "deleteClone"
+    ));
+
+    /**
+     * Maps every every block opcode to its corresponding Block Shape.
+     * Contains both Scratch 2 and 3 block opcodes.
+     */
     public static Map<String, BlockShape> opcodeShape;
     static {
         opcodeShape = new HashMap<>();
@@ -170,6 +327,9 @@ public class OpCodeMapping {
         }
     }
 
+    /**
+     * All available Block Shapes of Scratch.
+     */
     public enum BlockShape {
         HAT, STACK, BOOLEAN, REPORTER, C, CAP
     }
