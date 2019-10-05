60e874d0c1efd3050cd315571242f2f50dec4210
Nina <nina.nicole@gmx.de>
2019-10-05 14:01:18 +0200

    Add test for TurnDegreesTransformers

---
+++ src/test/java/scratch/structure/ast/stack/TurnDegreesBlockTest.java
@@ -0,0 +1,99 @@
+package scratch.structure.ast.stack;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import scratch.structure.ast.Ast;
+import scratch.structure.ast.BasicBlock;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class TurnDegreesBlockTest {
+
+    JsonNode script;
+
+    @Before
+    public void setup() {
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        System.out.println("Current relative path is: " + s);
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("./src/test/java/scratch/structure/ast/fixtures/turndegrees.json"));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);        //.append('\n');
+            }
+            JsonNode rootNode = mapper.readTree(sb.toString());
+
+            Iterator<JsonNode> elements = rootNode.get("targets").elements();
+            while (elements.hasNext()) {
+                JsonNode c = elements.next();
+                if (c.has("isStage") && !c.get("isStage").asBoolean() && c.has("blocks")) {
+                    script = c.get("blocks");
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testStructure() {
+        Ast ast = new Ast();
+        ast.parseScript(script);
+
+        BasicBlock root = ast.getRoot();
+        if (!(root instanceof TurnDegreesBlock)) {
+            fail("Result of this fixture should be a turndegrees block");
+        }
+
+        BasicBlock node = root;
+        int count = 0;
+        while(node.getNext() != null) {
+            count++;
+            node = (BasicBlock) node.getNext();
+        }
+        assertEquals("Two nodes expected", 2, count);
+    }
+
+    @Test
+    public void testIntInput() {
+        Ast ast = new Ast();
+        ast.parseScript(script);
+
+        BasicBlock root = ast.getRoot();
+        if (!(root instanceof TurnDegreesBlock)) {
+            fail("Result of this fixture should be a turndegrees block");
+        }
+        TurnDegreesBlock block = (TurnDegreesBlock) root;
+        assertEquals(40, block.getInputValue());
+    }
+
+    @Test
+    public void testVariableInput() {
+        Ast ast = new Ast();
+        ast.parseScript(script);
+
+        BasicBlock root = ast.getRoot();
+        if (!(root instanceof TurnDegreesBlock)) {
+            fail("Result of this fixture should be a turndegrees block");
+        }
+        TurnDegreesBlock block = (TurnDegreesBlock) root.getNext();
+
+        assertEquals("`jEk@4|i[#Fk?(8x)AV.-my variable", block.getInputVariableID());
+    }
+}
