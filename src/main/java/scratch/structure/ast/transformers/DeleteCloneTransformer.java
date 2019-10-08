c4bece0202fccb3b5df518680d78331ffde9c629
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-10-08 12:09:32 +0200

    Add missing Transformer for DeleteCloneBlock

---
+++ src/main/java/scratch/structure/ast/transformers/DeleteCloneTransformer.java
@@ -0,0 +1,38 @@
+package scratch.structure.ast.transformers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import scratch.structure.ast.Ast;
+import scratch.structure.ast.BasicBlock;
+import scratch.structure.ast.cap.DeleteCloneBlock;
+import scratch.structure.ast.hat.WhenFlagClickedBlock;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class DeleteCloneTransformer implements Transformer {
+
+    @Override
+    public Set<String> getIdentifiers() {
+       return new HashSet<>(Arrays.asList("control_delete_this_clone"));
+    }
+
+    @Override
+    public BasicBlock transform(JsonNode node, Ast ast) {
+        String opcode = node.get("opcode").toString().replaceAll("^\"|\"$", "");
+        boolean topLevel = node.get("topLevel").asBoolean();
+        boolean shadow = node.get("shadow").asBoolean();
+
+        DeleteCloneBlock block;
+        if (!topLevel) {
+            block = new DeleteCloneBlock(opcode, null, shadow, topLevel, 0, 0);
+        } else {
+            int x = node.get("x").intValue();
+            int y = node.get("x").intValue();
+            block = new DeleteCloneBlock(opcode, null, shadow, topLevel, x, y);
+        }
+
+        return block;
+
+    }
+}
