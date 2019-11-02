9de909e8e85ad7642e47c7850ebd98369da33a5a
Nina <nina.nicole@gmx.de>
2019-11-02 13:29:41 +0100

    Add value attribute to number class

--- src/main/java/scratch/newast/model/expression/num/Number.java
+++ src/main/java/scratch/newast/model/expression/num/Number.java
@@ -1,8 +1,13 @@
 package scratch.newast.model.expression.num;
 
+import java.math.BigInteger;
+
 public class Number implements NumExpr {
 
-    public Number() {
+    private BigInteger value;
+
+    public Number(BigInteger value) {
+        this.value = value;
     }
 
 }
