2d471a57fe2b8f9035950e1e7f586e5a5c156b8d
Christoph Frädrich <chris@treebug.de>
2019-11-02 18:59:42 +0100

    Add missing parameter in Identifier [WIP]

--- src/main/java/scratch/newast/model/variable/Identifier.java
+++ src/main/java/scratch/newast/model/variable/Identifier.java
@@ -1,8 +1,13 @@
 package scratch.newast.model.variable;
 
 public class Identifier implements Variable {
+    private String value; // TODO check if this is correct
 
-    public Identifier() {
+    public Identifier(String value) {
+        this.value = value;
     }
 
+    public String getValue() {
+        return value;
+    }
 }
