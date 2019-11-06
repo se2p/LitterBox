edb7982ebd009fc7e1378c61015f55848d4caeb2
Nina <nina.nicole@gmx.de>
2019-11-06 08:07:05 +0100

    Add NumExprOpcode

---
+++ src/main/java/scratch/newast/opcodes/NumExprOpcode.java
@@ -0,0 +1,18 @@
+package scratch.newast.opcodes;
+
+public enum NumExprOpcode {
+    operator_add, operator_subtract, operator_multiply, operator_divide,
+    operator_mod, operator_random, operator_round, operator_length,
+    data_lengthoflist, data_itemnumoflist, sensing_timer, sensing_dayssince2000,
+    sensing_current, sensing_distanceto, sensing_mousex, sensing_mousey,
+    sensing_loudness, operator_mathop;
+
+    public static boolean contains(String opcode) {
+        for (EventOpcode value : EventOpcode.values()) {
+            if (value.toString().equals(opcode)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
