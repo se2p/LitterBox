d0a34600ee0519733da52e0ee9ff6b8fcb013998
Nina <nina.nicole@gmx.de>
2019-11-07 09:31:39 +0100

    Add test for parseNumber

---
+++ src/test/java/scratch/structure/newast/parser/ExpressionParserTest.java
@@ -0,0 +1,26 @@
+package scratch.structure.newast.parser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.junit.Before;
+import org.junit.Test;
+import scratch.newast.model.expression.num.Number;
+import scratch.newast.parser.ExpressionParser;
+import utils.JsonParser;
+
+import static org.junit.Assert.assertEquals;
+
+public class ExpressionParserTest {
+    private static JsonNode script;
+
+    @Before
+    public void setup() {
+        script = JsonParser.getBlocksNodeFromJSON("./src/test/java/scratch/structure/ast/fixtures/movesteps.json");
+    }
+
+    @Test
+    public void testNumExpr() {
+        JsonNode inputs = script.get("EU(l=G6)z8NGlJFcx|fS").get("inputs");
+        Number result = ExpressionParser.parseNumber(inputs, 0);
+        assertEquals("10.0", String.valueOf(result.getValue()));
+    }
+}
