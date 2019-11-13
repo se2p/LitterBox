34c5c8a895024af85f1f9a147d98dd8480974267
Nina <nina.nicole@gmx.de>
2019-11-13 12:13:16 +0100

    Add BoolExpr opcodes (not complete)

--- src/main/java/scratch/newast/opcodes/StringExprOpcode.java
+++ src/main/java/scratch/newast/opcodes/StringExprOpcode.java
@@ -2,7 +2,8 @@ package scratch.newast.opcodes;
 
 public enum StringExprOpcode {
 
-    none;
+    operator_join, operator_letter_of, sensing_username, data_itemoflist;
+    // FIXME attribute of, resource of are missing
 
     public static boolean contains(String opcode) {
         for (StringExprOpcode value : StringExprOpcode.values()) {
