1c890223eec93d2cfde839de77645cec76b0b9d0
Nina <nina.nicole@gmx.de>
2019-11-13 12:07:19 +0100

    Add BoolExpr opcodes

--- src/main/java/scratch/newast/opcodes/BoolExprOpcode.java
+++ src/main/java/scratch/newast/opcodes/BoolExprOpcode.java
@@ -2,7 +2,9 @@ package scratch.newast.opcodes;
 
 public enum BoolExprOpcode {
 
-    none;
+    sensing_touchingobject, sensing_touchingcolor, sensing_coloristouchingcolor,
+    sensing_keypressed, sensing_mousedown, operator_gt, operator_lt, operator_equals,
+    operator_and, operator_or, operator_not, operator_contains, data_listcontainsitem;
 
     public static boolean contains(String opcode) {
         for (BoolExprOpcode value : BoolExprOpcode.values()) {
