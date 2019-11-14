a54b251899a3300aa879655e366885fbe1bc6f66
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-11-14 09:44:49 +0100

    Extend ElementChoiceParser

--- src/main/java/scratch/newast/parser/ElementChoiceParser.java
+++ src/main/java/scratch/newast/parser/ElementChoiceParser.java
@@ -1,11 +1,42 @@
 package scratch.newast.parser;
 
+import static scratch.newast.Constants.FIELDS_KEY;
+
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Preconditions;
+import scratch.newast.Constants;
 import scratch.newast.model.elementchoice.ElementChoice;
+import scratch.newast.model.elementchoice.Next;
+import scratch.newast.model.elementchoice.Prev;
+import scratch.newast.model.elementchoice.Random;
+import scratch.newast.model.elementchoice.WithId;
+import scratch.newast.model.variable.Identifier;
 
 public class ElementChoiceParser {
 
     public static ElementChoice parse(JsonNode current, JsonNode allBlocks) {
-        throw new RuntimeException("Not implemented yet");
+        Preconditions.checkNotNull(current);
+        Preconditions.checkNotNull(allBlocks);
+
+        //TODO this may not work for all blocks, needs testing
+        String blockMenuID = current.get(Constants.INPUTS_KEY).get(0).get(Constants.POS_INPUT_VALUE).asText();
+        JsonNode menu = allBlocks.get(blockMenuID);
+        String elementName = menu.get(FIELDS_KEY).get(0).get(0).asText();
+
+        StandardElemChoice standardElemChoice = StandardElemChoice.valueOf((elementName.split(" ")[0]));
+        switch (standardElemChoice) {
+            case random:
+                return new Random();
+            case next:
+                return new Next();
+            case previous:
+                return new Prev();
+            default:
+                return new WithId(new Identifier(elementName));
+        }
+    }
+
+    private enum StandardElemChoice {
+        random, next, previous
     }
 }
