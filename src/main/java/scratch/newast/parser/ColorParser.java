746750713df047e93692729e6edbe89dd63bf7e8
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-11-14 13:51:23 +0100

    Add ColorParser and TouchableParser

---
+++ src/main/java/scratch/newast/parser/ColorParser.java
@@ -0,0 +1,12 @@
+package scratch.newast.parser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import scratch.newast.model.graphiceffect.Color;
+
+public class ColorParser {
+
+    public static Color parseColor(JsonNode current, JsonNode allBlocks) {
+        throw new RuntimeException("Not implemented yet");
+    }
+
+}
---
+++ src/main/java/scratch/newast/parser/TouchableParser.java
@@ -0,0 +1,11 @@
+package scratch.newast.parser;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import scratch.newast.model.touchable.Touchable;
+
+public class TouchableParser {
+
+    public static Touchable parseTouchable(JsonNode current, JsonNode allBlocks) {
+        throw new RuntimeException("Not implemented yet");
+    }
+}
