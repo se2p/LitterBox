69ac68a6da5f251554f4b676c8f267302486a5eb
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-11-15 14:49:02 +0100

    Fix parsing error in ResourceParser
    In some weird cases the field for the filename of resources is simply missing and thus cannot be parsed. In this case we have to reconstruct the filename based on the assetId and dataformat.

--- src/main/java/scratch/newast/parser/ResourceParser.java
+++ src/main/java/scratch/newast/parser/ResourceParser.java
@@ -5,6 +5,8 @@ import com.google.common.base.Preconditions;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.logging.Logger;
+import scratch.newast.ParsingException;
 import scratch.newast.model.URI;
 import scratch.newast.model.resource.ImageResource;
 import scratch.newast.model.resource.Resource;
@@ -34,10 +36,30 @@ public class ResourceParser {
         Iterator<JsonNode> iter = resourceNode.elements();
         while (iter.hasNext()) {
             JsonNode node = iter.next();
-            ImageResource res = new ImageResource(new Identifier(node.get(NAME).textValue()),
-                    new URI(node.get(MD5EXT).textValue()));
+            ImageResource res = null;
+            try {
+                res = new ImageResource(new Identifier(node.get(NAME).textValue()),
+                    getURI(node));
+            } catch (ParsingException e) {
+                Logger.getGlobal().warning(e.getMessage());
+                continue;
+            }
             parsedRessources.add(res);
         }
         return parsedRessources;
+    }
+
+    private static URI getURI(JsonNode node) throws ParsingException {
+        //TODO this works but is not really beautiful
+        if (node.has(MD5EXT)) {
+            return new URI(node.get(MD5EXT).textValue());
+        } else if (node.has("assetId") && node.has("dataFormat")) {
+            String assetId = node.get("assetId").asText();
+            String dataFormat = node.get("dataFormat").asText();
+            String fileName = assetId + "." + dataFormat;
+            return new URI(fileName);
+        } else {
+            throw new ParsingException("Cannot parse URI of resource node " + node.textValue());
+        }
     }
 }
