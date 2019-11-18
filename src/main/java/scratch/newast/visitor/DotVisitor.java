e855c9ed81e027eb7d673de2fac40a0662d6a135
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-11-18 12:57:08 +0100

    Improve DotVisitor

--- src/main/java/scratch/newast/visitor/DotVisitor.java
+++ src/main/java/scratch/newast/visitor/DotVisitor.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.List;
+import scratch.newast.model.ASTLeaf;
 import scratch.newast.model.ASTNode;
 import scratch.newast.model.ScratchVisitor;
 
@@ -16,17 +17,38 @@ import scratch.newast.model.ScratchVisitor;
 public class DotVisitor implements ScratchVisitor {
 
     List<String> edges = new LinkedList<>();
+    long counter = 0;
 
     @Override
     public void visit(ASTNode node) {
-        String name = node.getUniqueName();
-        for (ASTNode child : node.getChildren()) {
-            String edge = name + " -> " + child.getUniqueName();
-            edges.add(edge);
-        }
+        if (node instanceof ASTLeaf) {
+            recordLeaf((ASTLeaf) node);
+        } else {
+            String name = String.valueOf(node.hashCode()); //FIXME this is a hack
+            String label = name + " [label = \"" + node.getUniqueName() + "\"];";
+            edges.add(label);
+            for (ASTNode child : node.getChildren()) {
+                String edge = name + " -> " + child.hashCode() + "";
+                edges.add(edge);
+            }
 
-        for (ASTNode child : node.getChildren()) {
-            child.accept(this);
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
+    }
+
+    public void recordLeaf(ASTLeaf node) {
+        String name = String.valueOf(node.hashCode());
+        String label = name + " [label = \"" + node.getUniqueName() + "\"];";
+        edges.add(label);
+        String[] simpleStrings = node.toSimpleStringArray();
+        for (String simpleString : simpleStrings) {
+            counter++;
+            String sLabel = counter + " [label = \"" + simpleString + "\"];";
+            edges.add(sLabel);
+            String edge = name + " -> " + counter;
+            edges.add(edge);
         }
     }
 
