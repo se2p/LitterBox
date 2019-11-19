bd4cc830b255c856f69e28c9544f4fdee7a22166
Christoph Frädrich <fraedric@fim.uni-passau.de>
2019-11-19 14:17:06 +0100

    Implement parsing of missing blocks

--- src/main/java/scratch/ast/parser/stmt/SpriteLookStmtParser.java
+++ src/main/java/scratch/ast/parser/stmt/SpriteLookStmtParser.java
@@ -18,15 +18,23 @@
  */
 package scratch.ast.parser.stmt;
 
+import static scratch.ast.Constants.FIELDS_KEY;
+import static scratch.ast.Constants.FIELD_VALUE;
 import static scratch.ast.Constants.OPCODE_KEY;
+import static scratch.ast.opcodes.SpriteLookStmtOpcode.looks_gotofrontback;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.base.Preconditions;
 import scratch.ast.ParsingException;
 import scratch.ast.model.elementchoice.ElementChoice;
+import scratch.ast.model.expression.num.Mult;
 import scratch.ast.model.expression.num.NumExpr;
+import scratch.ast.model.expression.num.Number;
 import scratch.ast.model.expression.string.StringExpr;
+import scratch.ast.model.statement.spritelook.ChangeLayerBy;
 import scratch.ast.model.statement.spritelook.ChangeSizeBy;
+import scratch.ast.model.statement.spritelook.GoToBackLayer;
+import scratch.ast.model.statement.spritelook.GoToFrontLayer;
 import scratch.ast.model.statement.spritelook.Hide;
 import scratch.ast.model.statement.spritelook.Say;
 import scratch.ast.model.statement.spritelook.SayForSecs;
@@ -86,9 +94,45 @@ public class SpriteLookStmtParser {
                 numExpr = NumExprParser.parseNumExpr(current, 0, allBlocks);
                 return new SetSizeTo(numExpr);
             case looks_gotofrontback:
+                return parseGoToLayer(current, allBlocks);
             case looks_goforwardbackwardlayers:
+                return parseGoForwardBackwardLayer(current, allBlocks);
+
             default:
                 throw new RuntimeException("Not implemented for opcode " + opcodeString);
+        }
+    }
+
+    private static SpriteLookStmt parseGoForwardBackwardLayer(JsonNode current, JsonNode allBlocks)
+        throws ParsingException {
+        JsonNode front_back = current.get(FIELDS_KEY).get("FORWARD_BACKWARD").get(FIELD_VALUE);
+
+        NumExpr num = NumExprParser.parseNumExpr(current, 0, allBlocks);
+
+        String layerOption = front_back.asText();
+        if (layerOption.equals("forward")) {
+            return new ChangeLayerBy(num);
+        } else if (layerOption.equals("backward")) {
+            NumExpr negated = new Mult(new Number(-1), num);
+            return new ChangeLayerBy(negated);
+        } else {
+            throw new ParsingException("Unknown option " + layerOption +
+                "+ when parsing block with opcode " + current.get(OPCODE_KEY));
+        }
+    }
+
+    private static SpriteLookStmt parseGoToLayer(JsonNode current, JsonNode allBlocks) throws ParsingException {
+        Preconditions.checkArgument(current.get(OPCODE_KEY).asText().equals(looks_gotofrontback.toString()));
+
+        JsonNode front_back = current.get(FIELDS_KEY).get("FRONT_BACK").get(FIELD_VALUE);
+        String layerOption = front_back.asText();
+        if (layerOption.equals("front")) {
+            return new GoToFrontLayer();
+        } else if (layerOption.equals("back")) {
+            return new GoToBackLayer();
+        } else {
+            throw new ParsingException("Unknown option " + layerOption +
+                "+ when parsing block with opcode " + current.get(OPCODE_KEY));
         }
     }
 }
