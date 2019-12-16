f5c625c64937297a2fbfa18e8c31deaa7a0058fa
obermuel <obermuel@fim.uni-passau.de>
2019-12-16 11:14:50 +0100

    implement EmptyBody

--- src/main/java/newanalytics/smells/EmptyBody.java
+++ src/main/java/newanalytics/smells/EmptyBody.java
@@ -18,75 +18,128 @@
  */
 package newanalytics.smells;
 
-import java.util.Arrays;
-import java.util.List;
 import newanalytics.IssueFinder;
 import newanalytics.IssueReport;
+import scratch.ast.model.ASTNode;
+import scratch.ast.model.ActorDefinition;
 import scratch.ast.model.Program;
-import scratch.data.ScBlock;
-import scratch.data.Script;
-import scratch.structure.Scriptable;
+import scratch.ast.model.statement.control.*;
+import scratch.ast.visitor.ScratchVisitor;
+import utils.Preconditions;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Checks for empty if or else bodies.
  */
-public class EmptyBody implements IssueFinder {
-
-    String name = "empty_body";
+public class EmptyBody implements IssueFinder, ScratchVisitor {
+    private static final String NOTE1 = "There are no condition blocks with empty body in your project.";
+    private static final String NOTE2 = "Some of condition blocks have an empty body.";
+    public static final String NAME = "empty_body";
+    public static final String SHORT_NAME = "emptybdy";
+    private boolean found = false;
+    private int count = 0;
+    private List<String> actorNames = new LinkedList<>();
+    private ActorDefinition currentActor;
 
     @Override
     public IssueReport check(Program program) {
-        /*
-        List<Scriptable> scriptables = new ArrayList<>();
-        scriptables.add(program.getStage());
-        scriptables.addAll(program.getSprites());
-        int count;
-        List<String> pos = new ArrayList<>();
-        for (Scriptable scable : scriptables) {
-            for (Script script : scable.getScripts()) {
-                if (program.getVersion().equals(Version.SCRATCH2)) {
-                    searchBlocks(scable, script, script.getBlocks(), pos, Identifier.LEGACY_IF.getValue(), Identifier.LEGACY_IF_ELSE.getValue());
-                } else if (program.getVersion().equals(Version.SCRATCH3)) {
-                    searchBlocks(scable, script, script.getBlocks(), pos, Identifier.IF.getValue(), Identifier.IF_ELSE.getValue());
-                }
-            }
-        }
-        count = pos.size();
-        String notes = "All 'if' blocks have a body.";
+        Preconditions.checkNotNull(program);
+        program.accept(this);
+        String notes = NOTE1;
         if (count > 0) {
-            notes = "Some 'if' blocks have no body.";
+            notes = NOTE2;
         }
-
-        return new IssueReport(name, count, pos, program.getPath(), notes);
-
-         */
-        throw new RuntimeException("not implemented");
-    }
-
-    private void searchBlocks(Scriptable scable, Script sc, List<ScBlock> blocks, List<String> pos, String ifId, String ifElseId) {
-        for (ScBlock b : blocks) {
-            if (b.getContent().startsWith(ifElseId)) {
-                if (b.getNestedBlocks() == null || b.getNestedBlocks().size() == 0) {
-                    pos.add(scable.getName() + " at " + Arrays.toString(sc.getPosition()));
-                }
-            } else {
-                if (b.getContent().startsWith(ifId)) {
-                    if (b.getNestedBlocks() == null || b.getNestedBlocks().size() == 0) {
-                        pos.add(scable.getName() + " at " + Arrays.toString(sc.getPosition()));
-                    }
-                }
-            }
-            if (b.getNestedBlocks() != null && b.getNestedBlocks().size() > 0) {
-                searchBlocks(scable, sc, b.getNestedBlocks(), pos, ifId, ifElseId);
-            }
-            if (b.getElseBlocks() != null && b.getElseBlocks().size() > 0) {
-                searchBlocks(scable, sc, b.getElseBlocks(), pos, ifId, ifElseId);
-            }
-        }
+        return new IssueReport(NAME, count, actorNames, notes);
     }
 
     @Override
     public String getName() {
-        return name;
+        return NAME;
+    }
+
+    @Override
+    public void visit(ActorDefinition actor) {
+        currentActor = actor;
+        if (!actor.getChildren().isEmpty()) {
+            for (ASTNode child : actor.getChildren()) {
+                child.accept(this);
+            }
+        }
+
+        if (found) {
+            found = false;
+            actorNames.add(currentActor.getIdent().getName());
+        }
+    }
+
+    @Override
+    public void visit(IfElseStmt node) {
+        if (node.getStmtList().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (node.getElseStmts().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (!node.getChildren().isEmpty()) {
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
+    }
+
+    @Override
+    public void visit(IfThenStmt node) {
+        if (node.getThenStmts().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (!node.getChildren().isEmpty()) {
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
+    }
+
+    @Override
+    public void visit(UntilStmt node) {
+        if (node.getStmtList().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (!node.getChildren().isEmpty()) {
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
+    }
+
+    @Override
+    public void visit(RepeatForeverStmt node) {
+        if (node.getStmtList().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (!node.getChildren().isEmpty()) {
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
+    }
+
+    @Override
+    public void visit(RepeatTimesStmt node) {
+        if (node.getStmtList().getStmts().getListOfStmt().isEmpty()) {
+            found = true;
+            count++;
+        }
+        if (!node.getChildren().isEmpty()) {
+            for (ASTNode child : node.getChildren()) {
+                child.accept(this);
+            }
+        }
     }
 }
---
+++ src/test/java/newanalytics/smells/EmptyBodyTest.java
@@ -0,0 +1,43 @@
+package newanalytics.smells;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import newanalytics.IssueReport;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import scratch.ast.ParsingException;
+import scratch.ast.model.Program;
+import scratch.ast.parser.ProgramParser;
+
+import java.io.File;
+import java.io.IOException;
+
+public class EmptyBodyTest {
+    private static Program empty;
+    private static Program emptyBodies;
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @BeforeAll
+    public static void setUp() throws IOException, ParsingException {
+
+        File f = new File("./src/test/fixtures/emptyProject.json");
+        empty = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+        f = new File("./src/test/fixtures/smells/emptyBodies.json");
+        emptyBodies = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+
+    }
+
+    @Test
+    public void testEmptyProgram() {
+        EmptyBody parameterName = new EmptyBody();
+        IssueReport report = parameterName.check(empty);
+        Assertions.assertEquals(0, report.getCount());
+    }
+
+    @Test
+    public void testEmptyBodies() {
+        EmptyBody parameterName = new EmptyBody();
+        IssueReport report = parameterName.check(emptyBodies);
+        Assertions.assertEquals(6, report.getCount());
+    }
+}
