2516abaac0fb7ca094a080367c46b145adb2b216
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-03-20 12:23:20 +0100

    remove prints

--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/parser/ScriptParser.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/parser/ScriptParser.java
@@ -98,7 +98,7 @@ public class ScriptParser {
             } catch (ParsingException | RuntimeException e) { // FIXME Runtime Exception is temporary for development and needs to be removed
                 Logger.getGlobal().warning("Could not parse block with ID " + blockID + " and opcode "
                         + current.get(OPCODE_KEY));
-                e.printStackTrace();
+               // e.printStackTrace();
                 if (e instanceof NullPointerException) {
                     throw e;
                 }
