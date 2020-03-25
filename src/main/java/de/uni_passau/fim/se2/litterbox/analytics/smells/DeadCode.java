9058e52a2f720bf101bbc554c8c0d747a43384ce
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-03-25 12:33:45 +0100

    refactor DeadCode to use ScratchVisitor and add Test

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/smells/DeadCode.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/smells/DeadCode.java
@@ -20,23 +20,32 @@ package de.uni_passau.fim.se2.litterbox.analytics.smells;
 
 import de.uni_passau.fim.se2.litterbox.analytics.IssueFinder;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueReport;
+import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import de.uni_passau.fim.se2.litterbox.ast.model.Script;
 import de.uni_passau.fim.se2.litterbox.ast.model.event.Never;
+import de.uni_passau.fim.se2.litterbox.ast.visitor.ScratchVisitor;
 import de.uni_passau.fim.se2.litterbox.utils.Preconditions;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Checks if the project has loose blocks without a head.
  */
-public class DeadCode implements IssueFinder {
+public class DeadCode implements IssueFinder, ScratchVisitor {
 
     private static final String NOTE1 = "There are no loose blocks in your project.";
     private static final String NOTE2 = "Some of the Sprites have loose blocks!";
     public static final String NAME = "dead_code";
     public static final String SHORT_NAME = "dcode";
+    private boolean found = false;
+    private int count = 0;
+    private List<String> actorNames = new LinkedList<>();
+    private ActorDefinition currentActor;
+    private Program program;
 
     public DeadCode() {
     }
@@ -44,22 +53,37 @@ public class DeadCode implements IssueFinder {
     @Override
     public IssueReport check(Program program) {
         Preconditions.checkNotNull(program);
-        List<String> found = new ArrayList<>();
-        final List<ActorDefinition> definitions = program.getActorDefinitionList().getDefintions();
-        for (ActorDefinition actor : definitions) {
-            List<Script> scripts = actor.getScripts().getScriptList();
-            for (Script current : scripts) {
-                if ((current.getEvent() instanceof Never) && current.getStmtList().getStmts().getListOfStmt().size() >= 1) {
-                    found.add(actor.getIdent().getName());
-                }
-            }
-        }
+        found = false;
+        count = 0;
+        actorNames = new LinkedList<>();
+        this.program = program;
+        program.accept(this);
         String notes = NOTE1;
-        if (found.size() > 0) {
+        if (count > 0) {
             notes = NOTE2;
         }
+        return new IssueReport(NAME, count, actorNames, notes);
+    }
 
-        return new IssueReport(NAME, found.size(), found, notes);
+    @Override
+    public void visit(ActorDefinition actor) {
+        currentActor = actor;
+        if (!actor.getChildren().isEmpty()) {
+            for (ASTNode child : actor.getChildren()) {
+                child.accept(this);
+            }
+        }
+        if (found) {
+            found = false;
+            actorNames.add(currentActor.getIdent().getName());
+        }
+    }
+
+    @Override
+    public void visit(Script node) {
+        if (node.getEvent() instanceof Never && node.getStmtList().getStmts().getListOfStmt().size() > 0) {
+            count++;
+        }
     }
 
     @Override
