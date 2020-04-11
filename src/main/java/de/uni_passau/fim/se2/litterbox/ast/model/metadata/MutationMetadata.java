e2fbba665ff037496864c9ed072fac6aa7f62f15
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-04-11 12:26:37 +0200

    add attributes MutationMetadata

--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/MutationMetadata.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/MutationMetadata.java
@@ -4,6 +4,53 @@ import de.uni_passau.fim.se2.litterbox.ast.model.AbstractNode;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ScratchVisitor;
 
 public class MutationMetadata extends AbstractNode implements Metadata {
+    private String tagName;
+    private String[] children;
+    private String procCode;
+    private String argumentIds;
+    private String argumentNames;
+    private String argumentDefaults;
+    private boolean warp;
+
+    public MutationMetadata(String tagName, String[] children, String procCode, String argumentIds,
+                            String argumentNames, String argumentDefaults, boolean warp) {
+        super();
+        this.tagName = tagName;
+        this.children = children;
+        this.procCode = procCode;
+        this.argumentIds = argumentIds;
+        this.argumentNames = argumentNames;
+        this.argumentDefaults = argumentDefaults;
+        this.warp = warp;
+    }
+
+    public String getTagName() {
+        return tagName;
+    }
+
+    public String[] getChild() {
+        return children;
+    }
+
+    public String getProcCode() {
+        return procCode;
+    }
+
+    public String getArgumentIds() {
+        return argumentIds;
+    }
+
+    public String getArgumentNames() {
+        return argumentNames;
+    }
+
+    public String getArgumentDefaults() {
+        return argumentDefaults;
+    }
+
+    public boolean isWarp() {
+        return warp;
+    }
 
     @Override
     public void accept(ScratchVisitor visitor) {
