17c7fe6e5cdbad4cc991a8a2d447555601239419
Nina <nina.nicole@gmx.de>
2020-04-15 13:02:17 +0200

    Propagate rename

--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/ImageMetadata.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/ImageMetadata.java
@@ -2,7 +2,7 @@ package de.uni_passau.fim.se2.litterbox.ast.model.metadata;
 
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ScratchVisitor;
 
-public class ImageMetadata extends RessourceMetadata {
+public class ImageMetadata extends ResourceMetadata {
 
     private int bitmapResolution;
     private double rotationCenterX;
