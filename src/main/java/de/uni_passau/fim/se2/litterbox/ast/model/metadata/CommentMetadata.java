313f472b3008f1d200fcd0842a20668755110ede
Nina <nina.nicole@gmx.de>
2020-04-15 13:08:09 +0200

    Add CommentMetadata attributes

--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/CommentMetadata.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/CommentMetadata.java
@@ -4,6 +4,15 @@ import de.uni_passau.fim.se2.litterbox.ast.model.AbstractNode;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ScratchVisitor;
 
 public class CommentMetadata extends AbstractNode implements Metadata {
+
+    private String blockId;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private boolean minimized;
+    private String text;
+
     @Override
     public void accept(ScratchVisitor visitor) {
         visitor.visit(this);
