2e3cc8539c33a7f4df226cf21c8756ccd981cfd1
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-04-30 18:37:05 +0200

    implement ImageMetadataTest and make bitmapResolution a Double so it can be null

--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/ressources/ImageMetadata.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/model/metadata/ressources/ImageMetadata.java
@@ -3,11 +3,11 @@ package de.uni_passau.fim.se2.litterbox.ast.model.metadata.ressources;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ScratchVisitor;
 
 public class ImageMetadata extends ResourceMetadata {
-    private double bitmapResolution;
+    private Double bitmapResolution;
     private double rotationCenterX;
     private double rotationCenterY;
 
-    public ImageMetadata(String assetId, String name, String md5ext, String dataFormat, double bitmapResolution,
+    public ImageMetadata(String assetId, String name, String md5ext, String dataFormat, Double bitmapResolution,
                          double rotationCenterX, double rotationCenterY) {
         super(assetId, name, md5ext, dataFormat);
         this.bitmapResolution = bitmapResolution;
@@ -15,7 +15,7 @@ public class ImageMetadata extends ResourceMetadata {
         this.rotationCenterY = rotationCenterY;
     }
 
-    public double getBitmapResolution() {
+    public Double getBitmapResolution() {
         return bitmapResolution;
     }
 
--- src/main/java/de/uni_passau/fim/se2/litterbox/ast/parser/metadata/ImageMetadataParser.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/ast/parser/metadata/ImageMetadataParser.java
@@ -8,11 +8,14 @@ public class ImageMetadataParser {
     public static ImageMetadata parse(JsonNode imageNode) {
         String assetId = imageNode.get("assetId").asText();
         String name = imageNode.get("name").asText();
-        String md5ext= imageNode.get("md5ext").asText();
-        String dataFormat= imageNode.get("dataFormat").asText();
-        double bitmapResolution= imageNode.get("bitmapResolution").asDouble();
-        double rotationCenterX= imageNode.get("rotationCenterX").asDouble();
-        double rotationCenterY= imageNode.get("rotationCenterY").asDouble();
-        return new ImageMetadata(assetId,name,md5ext,dataFormat,bitmapResolution,rotationCenterX,rotationCenterY);
+        String md5ext = imageNode.get("md5ext").asText();
+        String dataFormat = imageNode.get("dataFormat").asText();
+        Double bitmapResolution = null;
+        if (imageNode.has("bitmapResolution")) {
+            bitmapResolution = imageNode.get("bitmapResolution").asDouble();
+        }
+        double rotationCenterX = imageNode.get("rotationCenterX").asDouble();
+        double rotationCenterY = imageNode.get("rotationCenterY").asDouble();
+        return new ImageMetadata(assetId, name, md5ext, dataFormat, bitmapResolution, rotationCenterX, rotationCenterY);
     }
 }
