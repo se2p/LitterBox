5d5be316a5cb917e51fe1edc67a3ae09d679b97c
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-05-25 16:36:08 +0200

    add helper methods for writing fixed blocks and the topLevel, shadow, comment, x and y writing

--- src/main/java/de/uni_passau/fim/se2/litterbox/jsonCreation/ScriptJSONCreator.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/jsonCreation/ScriptJSONCreator.java
@@ -4,11 +4,9 @@ import de.uni_passau.fim.se2.litterbox.ast.model.Script;
 import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
 import de.uni_passau.fim.se2.litterbox.ast.model.event.*;
 import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.TopNonDataBlockMetadata;
-import de.uni_passau.fim.se2.litterbox.ast.model.statement.Stmt;
 
-import java.util.List;
-
-import static de.uni_passau.fim.se2.litterbox.ast.Constants.*;
+import static de.uni_passau.fim.se2.litterbox.jsonCreation.StmtListJSONCreator.createBlockUpToParent;
+import static de.uni_passau.fim.se2.litterbox.jsonCreation.StmtListJSONCreator.createFixedBlock;
 
 public class ScriptJSONCreator {
     public static String createScriptJSONString(Script script) {
@@ -22,58 +20,66 @@ public class ScriptJSONCreator {
         } else {
             StringBuilder endOfEventBlock = new StringBuilder();
             String blockId = null;
+            String nextId = null;
+
+            if (stmtList.getStmts().size() > 0) {
+                IdVisitor vis = new IdVisitor();
+                nextId = vis.getBlockId(stmtList.getStmts().get(0));
+            }
+
             if (event instanceof AttributeAboveValue) {
                 AttributeAboveValue attributeAboveValue = (AttributeAboveValue) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) attributeAboveValue.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createBlockUpToParent(jsonString, meta, nextId, null);
             } else if (event instanceof BackdropSwitchTo) {
                 BackdropSwitchTo backdropSwitchTo = (BackdropSwitchTo) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) backdropSwitchTo.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createBlockUpToParent(jsonString, meta, nextId, null);
             } else if (event instanceof GreenFlag) {
                 GreenFlag greenFlag = (GreenFlag) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) greenFlag.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createFixedBlock(jsonString, meta, nextId, null);
             } else if (event instanceof KeyPressed) {
                 KeyPressed keyPressed = (KeyPressed) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) keyPressed.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createBlockUpToParent(jsonString, meta, nextId, null);
             } else if (event instanceof ReceptionOfMessage) {
                 ReceptionOfMessage receptionOfMessage = (ReceptionOfMessage) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) receptionOfMessage.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createBlockUpToParent(jsonString, meta, nextId, null);
             } else if (event instanceof SpriteClicked) {
                 SpriteClicked spriteClicked = (SpriteClicked) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) spriteClicked.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createFixedBlock(jsonString, meta, nextId, null);
             } else if (event instanceof StageClicked) {
                 StageClicked stageClicked = (StageClicked) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) stageClicked.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createFixedBlock(jsonString, meta, nextId, null);
             } else if (event instanceof StartedAsClone) {
                 StartedAsClone startedAsClone = (StartedAsClone) event;
                 TopNonDataBlockMetadata meta = (TopNonDataBlockMetadata) startedAsClone.getMetadata();
                 //todo event handling
                 blockId = meta.getBlockId();
-                createBlockUpToParent(jsonString,meta,stmtList);
+                createFixedBlock(jsonString, meta, nextId, null);
             }
             if (script.getStmtList().getStmts().size() > 0) {
                 assert blockId != null;
-                stmtListJSONCreator = new StmtListJSONCreator(blockId,stmtList);
+                stmtListJSONCreator = new StmtListJSONCreator(blockId, stmtList);
+                jsonString.append(",");
                 jsonString.append(stmtListJSONCreator.getStmtListJSONString());
             }
         }
@@ -81,19 +87,5 @@ public class ScriptJSONCreator {
         return jsonString.toString();
     }
 
-    private static void createBlockUpToParent(StringBuilder jsonString, TopNonDataBlockMetadata meta,
-                                              StmtList stmtList) {
 
-        JSONStringCreator.createField(jsonString, meta.getBlockId()).append("{");
-        JSONStringCreator.createFieldValue(jsonString, OPCODE_KEY, meta.getOpcode()).append(",");
-        List<Stmt> stmts = stmtList.getStmts();
-        if (stmts.size() == 0) {
-            JSONStringCreator.createFieldValueNull(jsonString, NEXT_KEY).append(",");
-        } else {
-            IdVisitor vis = new IdVisitor();
-            String nextId = vis.getBlockId(stmts.get(0));
-            JSONStringCreator.createFieldValue(jsonString, NEXT_KEY, nextId).append(",");
-        }
-        JSONStringCreator.createFieldValueNull(jsonString, PARENT_KEY).append(",");
-    }
 }
--- src/main/java/de/uni_passau/fim/se2/litterbox/jsonCreation/StmtListJSONCreator.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/jsonCreation/StmtListJSONCreator.java
@@ -1,9 +1,14 @@
 package de.uni_passau.fim.se2.litterbox.jsonCreation;
 
 import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
+import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.NonDataBlockMetadata;
+import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.TopNonDataBlockMetadata;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static de.uni_passau.fim.se2.litterbox.ast.Constants.*;
+import static de.uni_passau.fim.se2.litterbox.jsonCreation.JSONStringCreator.*;
 
 public class StmtListJSONCreator {
     private boolean startsWithTopBlock = false;
@@ -22,5 +27,54 @@ public class StmtListJSONCreator {
 
     public String getStmtListJSONString() {
         return "";
+    }
+
+    public static StringBuilder createBlockUpToParent(StringBuilder jsonString, NonDataBlockMetadata meta,
+                                                      String nextId, String parentId) {
+
+        createField(jsonString, meta.getBlockId()).append("{");
+        createFieldValue(jsonString, OPCODE_KEY, meta.getOpcode()).append(",");
+        if (nextId == null) {
+            createFieldValueNull(jsonString, NEXT_KEY).append(",");
+        } else {
+            createFieldValue(jsonString, NEXT_KEY, nextId).append(",");
+        }
+        if (parentId == null) {
+            createFieldValueNull(jsonString, PARENT_KEY).append(",");
+        } else {
+            createFieldValue(jsonString, PARENT_KEY, parentId).append(",");
+        }
+        return jsonString;
+    }
+
+    public static StringBuilder createBlockInputFieldForFixed(StringBuilder jsonString) {
+        createField(jsonString, INPUTS_KEY).append("{},");
+        createField(jsonString, FIELDS_KEY).append("{},");
+        return jsonString;
+    }
+
+    public static StringBuilder createBlockAfterFields(StringBuilder jsonString, NonDataBlockMetadata meta) {
+        createFieldValue(jsonString, SHADOW_KEY, meta.isShadow()).append(",");
+        createFieldValue(jsonString, TOPLEVEL_KEY, meta.isTopLevel());
+        if (meta.getCommentId() != null) {
+            jsonString.append(",");
+            createFieldValue(jsonString, COMMENT_KEY, meta.getCommentId());
+        }
+        if (meta instanceof TopNonDataBlockMetadata) {
+            TopNonDataBlockMetadata topNonDataBlockMetadata = (TopNonDataBlockMetadata) meta;
+            jsonString.append(",");
+            createFieldValue(jsonString, X_KEY, topNonDataBlockMetadata.getxPos()).append(",");
+            createFieldValue(jsonString, Y_KEY, topNonDataBlockMetadata.getyPos());
+        }
+        jsonString.append("}");
+        return jsonString;
+    }
+
+    public static StringBuilder createFixedBlock(StringBuilder jsonString, NonDataBlockMetadata meta,
+                                                 String nextId, String parentId) {
+        createBlockUpToParent(jsonString, meta, nextId, parentId);
+        createBlockInputFieldForFixed(jsonString);
+        createBlockAfterFields(jsonString, meta);
+        return jsonString;
     }
 }
