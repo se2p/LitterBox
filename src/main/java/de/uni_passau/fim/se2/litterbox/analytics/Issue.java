0d1caaaa2c7e8dcfd150b2f7cfb15def59d62f70
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-06-30 11:15:35 +0200

    remove TODO

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Issue.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Issue.java
@@ -77,7 +77,7 @@ public class Issue {
     }
 
     public String getActorName() {
-        return actor.getIdent().getName(); // TODO
+        return actor.getIdent().getName();
     }
 
     public String getFinderName() {
