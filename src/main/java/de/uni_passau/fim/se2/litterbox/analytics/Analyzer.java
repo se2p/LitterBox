eafce3810b3b58c85f496e71912535c65537f3f8
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-07-10 08:57:31 +0200

    Pull up methods from BugAnalyzer to Analyzer

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Analyzer.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Analyzer.java
@@ -1,9 +1,28 @@
 package de.uni_passau.fim.se2.litterbox.analytics;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.parser.ProgramParser;
+import de.uni_passau.fim.se2.litterbox.utils.Downloader;
+import de.uni_passau.fim.se2.litterbox.utils.JsonParser;
+import de.uni_passau.fim.se2.litterbox.utils.ZipReader;
+import org.apache.commons.io.FilenameUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 public abstract class Analyzer {
+
+    private static final Logger log = Logger.getLogger(Analyzer.class.getName());
 
     Path input;
     String output;
@@ -13,9 +32,93 @@ public abstract class Analyzer {
         this.output = output;
     }
 
-    public abstract void analyzeFile();
+    public void analyzeFile() {
+        File file = input.toFile();
 
-    public abstract void analyzeMultiple(String listPath);
+        if (file.exists() && file.isDirectory()) {
+            for (final File fileEntry : Objects.requireNonNull(file.listFiles())) {
+                if (!fileEntry.isDirectory()) {
+                    check(fileEntry, String.valueOf(output));
+                }
+            }
+        } else if (file.exists() && !file.isDirectory()) {
+            check(file, String.valueOf(output));
+        } else {
+            log.info("Folder or file '" + file.getName() + "' does not exist");
+        }
+    }
 
-    public abstract void analyzeSingle(String listPath);
+    public void analyzeMultiple(String listPath) {
+        Path projectList = Paths.get(listPath);
+
+        try {
+            List<String> pids = Files.lines(projectList).collect(Collectors.toList());
+            for (String pid : pids) {
+                analyzeSingle(pid);
+            }
+        } catch (IOException e) {
+            log.warning("Could not read project list at " + projectList.toString());
+            e.printStackTrace();
+        }
+    }
+
+    public void analyzeSingle(String pid) {
+        Path path = Paths.get(input.toString(), pid + ".json");
+        File projectFile = path.toFile();
+        if (!projectFile.exists()) {
+            try {
+                Downloader.downloadAndSaveProject(pid, input.toString());
+            } catch (IOException e) {
+                log.warning("Could not download project with PID: " + pid);
+                e.printStackTrace();
+            }
+        }
+
+        check(projectFile, output);
+    }
+
+    abstract void check(File fileEntry, String csv);
+
+    /**
+     * Extracts a Scratch Program from a Json or sb3 file
+     *
+     * @param fileEntry of the json or sb3 file
+     * @return the parsed program or null in case the program could not be loaded or parsed
+     */
+    protected Program extractProgram(File fileEntry) {
+        ObjectMapper mapper = new ObjectMapper();
+        Program program;
+
+        String fileName = fileEntry.getName();
+        String programName = fileName.substring(0, fileName.lastIndexOf("."));
+        JsonNode programNode = null;
+
+        try {
+            if ((FilenameUtils.getExtension(fileEntry.getPath())).toLowerCase().equals("json")) {
+                programNode = mapper.readTree(fileEntry);
+            } else {
+                programNode = JsonParser.getTargetsNodeFromJSONString(ZipReader.getJsonString(fileEntry.getPath()));
+            }
+        } catch (IOException e) {
+            log.info("[Error] could not load program from file");
+            e.printStackTrace();
+        }
+
+        if (programNode == null) {
+            // TODO: Proper error handling
+            log.info("[Error] project json did not contain root node");
+            return null;
+        }
+
+        try {
+            program = ProgramParser.parseProgram(programName, programNode);
+        } catch (ParsingException | RuntimeException e) {
+            // TODO: Proper error handling
+            log.info("[Error] could not parse program");
+            e.printStackTrace();
+            return null;
+        }
+
+        return program;
+    }
 }
--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/BugAnalyzer.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/BugAnalyzer.java
@@ -1,28 +1,14 @@
 package de.uni_passau.fim.se2.litterbox.analytics;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
-import de.uni_passau.fim.se2.litterbox.ast.parser.ProgramParser;
 import de.uni_passau.fim.se2.litterbox.report.CSVReportGenerator;
 import de.uni_passau.fim.se2.litterbox.report.ConsoleReportGenerator;
-import de.uni_passau.fim.se2.litterbox.utils.Downloader;
 import de.uni_passau.fim.se2.litterbox.utils.GroupConstants;
-import de.uni_passau.fim.se2.litterbox.utils.JsonParser;
-import de.uni_passau.fim.se2.litterbox.utils.ZipReader;
-import org.apache.commons.io.FilenameUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
 import static de.uni_passau.fim.se2.litterbox.utils.GroupConstants.*;
 
@@ -36,53 +22,6 @@ public class BugAnalyzer extends Analyzer {
     public BugAnalyzer(String input, String output) {
         super(input, output);
         this.issueTool = new IssueTool();
-    }
-
-    public void analyzeFile() {
-        File file = input.toFile();
-
-        if (file.exists() && file.isDirectory()) {
-            for (final File fileEntry : Objects.requireNonNull(file.listFiles())) {
-                if (!fileEntry.isDirectory()) {
-                    check(fileEntry, String.valueOf(output));
-                }
-            }
-        } else if (file.exists() && !file.isDirectory()) {
-            check(file, String.valueOf(output));
-        } else {
-            log.info("Folder or file '" + file.getName() + "' does not exist");
-        }
-    }
-
-    @Override
-    public void analyzeMultiple(String listPath) {
-        Path projectList = Paths.get(listPath);
-
-        try {
-            List<String> pids = Files.lines(projectList).collect(Collectors.toList());
-            for (String pid : pids) {
-                analyzeSingle(pid);
-            }
-        } catch (IOException e) {
-            log.warning("Could not read project list at " + projectList.toString());
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void analyzeSingle(String pid) {
-        Path path = Paths.get(input.toString(), pid + ".json");
-        File projectFile = path.toFile();
-        if (!projectFile.exists()) {
-            try {
-                Downloader.downloadAndSaveProject(pid, input.toString());
-            } catch (IOException e) {
-                log.warning("Could not download project with PID: " + pid);
-                e.printStackTrace();
-            }
-        }
-
-        check(projectFile, output);
     }
 
     public void setDetectorNames(String detectorNames) {
@@ -107,7 +46,7 @@ public class BugAnalyzer extends Analyzer {
      *
      * @param fileEntry the file to analyze
      */
-    private void check(File fileEntry, String csv) {
+    void check(File fileEntry, String csv) {
         Program program = extractProgram(fileEntry);
         if (program == null) {
             // Todo error message
@@ -128,48 +67,5 @@ public class BugAnalyzer extends Analyzer {
         } catch (IOException e) {
             log.warning(e.getMessage());
         }
-    }
-
-    /**
-     * Extracts a Scratch Program from a Json or sb3 file
-     *
-     * @param fileEntry of the json or sb3 file
-     * @return the parsed program or null in case the program could not be loaded or parsed
-     */
-    private Program extractProgram(File fileEntry) {
-        ObjectMapper mapper = new ObjectMapper();
-        Program program;
-
-        String fileName = fileEntry.getName();
-        String programName = fileName.substring(0, fileName.lastIndexOf("."));
-        JsonNode programNode = null;
-
-        try {
-            if ((FilenameUtils.getExtension(fileEntry.getPath())).toLowerCase().equals("json")) {
-                programNode = mapper.readTree(fileEntry);
-            } else {
-                programNode = JsonParser.getTargetsNodeFromJSONString(ZipReader.getJsonString(fileEntry.getPath()));
-            }
-        } catch (IOException e) {
-            log.info("[Error] could not load program from file");
-            e.printStackTrace();
-        }
-
-        if (programNode == null) {
-            // TODO: Proper error handling
-            log.info("[Error] project json did not contain root node");
-            return null;
-        }
-
-        try {
-            program = ProgramParser.parseProgram(programName, programNode);
-        } catch (ParsingException | RuntimeException e) {
-            // TODO: Proper error handling
-            log.info("[Error] could not parse program");
-            e.printStackTrace();
-            return null;
-        }
-
-        return program;
     }
 }
