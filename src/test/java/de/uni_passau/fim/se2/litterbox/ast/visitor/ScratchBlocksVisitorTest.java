bb9165653a68dbb1bf3559aaf4aee63688bf308c
Gordon Fraser <gordon.fraser@uni-passau.de>
2020-07-12 14:34:39 +0200

    Add test for look blocks in ScratchBlocks

--- src/test/java/de/uni_passau/fim/se2/litterbox/ast/visitor/ScratchBlocksVisitorTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox/ast/visitor/ScratchBlocksVisitorTest.java
@@ -12,6 +12,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class ScratchBlocksVisitorTest {
 
@@ -31,7 +33,53 @@ public class ScratchBlocksVisitorTest {
         ScratchBlocksVisitor visitor = new ScratchBlocksVisitor(ps);
         program.accept(visitor);
         String result = os.toString();
-        System.out.println(result);
+        assertEquals("[scratchblocks]\n" +
+                "when green flag clicked\n" +
+                "move (10) steps\n" +
+                "turn right (15) degrees\n" +
+                "turn left (15) degrees\n" +
+                "go to (random position v)\n" +
+                "go to x: (0) y: (0)\n" +
+                "glide (1) secs to (random position v)\n" +
+                "glide (1) secs to x: (0) y: (0)\n" +
+                "point in direction (90)\n" +
+                "point towards (mouse-pointer v)\n" +
+                "change x by (10)\n" +
+                "set x to (0)\n" +
+                "change y by (10)\n" +
+                "set y to (0)\n" +
+                "if on edge, bounce\n" +
+                "set rotation style [left-right v]\n" +
+                "[/scratchblocks]\n", result);
     }
 
+    @Test
+    public void testLookBlocks() throws IOException, ParsingException {
+        Program program = getAST("src/test/fixtures/scratchblocks/lookblocks.json");
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(os);
+        ScratchBlocksVisitor visitor = new ScratchBlocksVisitor(ps);
+        program.accept(visitor);
+        String result = os.toString();
+        assertEquals("[scratchblocks]\n" +
+                "when green flag clicked\n" +
+                "say [Hello!] for (2) seconds\n" +
+                "say [Hello!]\n" +
+                "think [Hmm...] for (2) seconds\n" +
+                "think [Hmm...]\n" +
+                "switch costume to (costume2 v)\n" +
+                "next costume\n" +
+                "switch backdrop to (backdrop1 v)\n" +
+                "next backdrop\n" +
+                "change size by (10)\n" +
+                "set size to (100) %\n" +
+                "change [color v] effect by (25)\n" +
+                "set [color v] effect to (0)\n" +
+                "clear graphic effects\n" +
+                "show\n" +
+                "hide\n" +
+                "go to [front v] layer\n" +
+                "go [forward v] (1) layers\n" +
+                "[/scratchblocks]\n", result);
+    }
 }
