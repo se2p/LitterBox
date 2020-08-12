fe5eeef11376de52bc1f9a407de50bc7f9c1b288
Christoph Frädrich <fraedric@fim.uni-passau.de>
2020-08-12 08:56:33 +0200

    Simplify issue finder
    Nina made the great observation that this issue can be identified by simply looking at the statement list of loops and checking if the last block is a stop block. If the block was guarded by a an if-block the if-block would be the last block in the statement list of the loop, not the stop block. So any stop block that is the last is unguarded and thus this is an instance of this issue.

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/bugpattern/TerminatedLoop.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/bugpattern/TerminatedLoop.java
@@ -3,29 +3,31 @@ package de.uni_passau.fim.se2.litterbox.analytics.bugpattern;
 import de.uni_passau.fim.se2.litterbox.analytics.AbstractIssueFinder;
 import de.uni_passau.fim.se2.litterbox.analytics.Issue;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
-import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.*;
+import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.Stmt;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.RepeatForeverStmt;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.RepeatTimesStmt;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.UntilStmt;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.termination.StopAll;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.termination.StopThisScript;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * TerminatedLoop is a bug pattern which occurs if a loop contains a StopAll or StopThisScript block which
- * is not guarded by some condition. If that is the case the loop will only execute once and all blocks succeeding
- * the loop (in the case of a "repeat times" or "repeat until") will not be executed either.
+ * is not guarded by some condition. This can be identified simply looking at the last child in the statement list of a
+ * loop and checking if it is a stop block. If that is the case the loop will only execute once and all blocks
+ * succeeding the loop (in the case of a "repeat times" or "repeat until") will not be executed either.
  */
 public class TerminatedLoop extends AbstractIssueFinder {
 
     private static final String NAME = "terminated_loop";
-    private boolean inLoop = false;
-    private boolean inIf = false;
 
     @Override
     public Set<Issue> check(Program program) {
         issues = new LinkedHashSet<>();
-        inLoop = false;
-        inIf = false;
 
         return super.check(program);
     }
@@ -41,58 +43,33 @@ public class TerminatedLoop extends AbstractIssueFinder {
     }
 
     @Override
-    public void visit(IfElseStmt node) {
-        inIf = true;
-        visitChildren(node);
-        inIf = false;
-    }
-
-    @Override
-    public void visit(IfThenStmt node) {
-        inIf = true;
-        visitChildren(node);
-        inIf = false;
-    }
-
-    @Override
-    public void visit(IfStmt node) {
-        inIf = true;
-        visitChildren(node);
-        inIf = false;
-    }
-
-    @Override
     public void visit(RepeatForeverStmt node) {
-        inLoop = true;
+        StmtList children = node.getStmtList();
+        checkLoopChildren(children);
         visitChildren(node);
-        inLoop = false;
+    }
+
+    private void checkLoopChildren(StmtList children) {
+        List<Stmt> stmts = children.getStmts();
+        Stmt last = stmts.get(stmts.size() - 1);
+        if (last instanceof StopAll) {
+            addIssue(last, ((StopAll) last).getMetadata());
+        } else if (last instanceof StopThisScript) {
+            addIssue(last, ((StopThisScript) last).getMetadata());
+        }
     }
 
     @Override
     public void visit(RepeatTimesStmt node) {
-        inLoop = true;
+        StmtList children = node.getStmtList();
+        checkLoopChildren(children);
         visitChildren(node);
-        inLoop = false;
     }
 
     @Override
     public void visit(UntilStmt node) {
-        inLoop = true;
+        StmtList children = node.getStmtList();
+        checkLoopChildren(children);
         visitChildren(node);
-        inLoop = false;
-    }
-
-    @Override
-    public void visit(StopAll node) {
-        if (inLoop && !inIf) {
-            addIssue(node, node.getMetadata());
-        }
-    }
-
-    @Override
-    public void visit(StopThisScript node) {
-        if (inLoop && !inIf) {
-            addIssue(node, node.getMetadata());
-        }
     }
 }
