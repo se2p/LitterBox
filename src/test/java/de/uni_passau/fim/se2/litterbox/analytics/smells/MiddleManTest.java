c2ee9dd0f10a295910a5e9f37325f18734308fa2
Nina <nina.nicole@gmx.de>
2020-09-11 08:18:47 +0200

    Remove global fixtures from test class

--- src/test/java/de/uni_passau/fim/se2/litterbox/analytics/smells/MiddleManTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox/analytics/smells/MiddleManTest.java
@@ -24,7 +24,6 @@ import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import de.uni_passau.fim.se2.litterbox.ast.parser.ProgramParser;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -32,30 +31,25 @@ import java.io.IOException;
 import java.util.Set;
 
 public class MiddleManTest {
-    private static Program empty;
-    private static Program middleMan;
-    private static ObjectMapper mapper = new ObjectMapper();
-
-    @BeforeAll
-    public static void setUp() throws IOException, ParsingException {
-
-        File f = new File("./src/test/fixtures/emptyProject.json");
-        empty = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-        f = new File("./src/test/fixtures/smells/middleMan.json");
-        middleMan = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
-    }
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void testEmptyProgram() {
+    public void testEmptyProgram() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/emptyProject.json");
+        Program empty = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+
         MiddleMan finder = new MiddleMan();
         Set<Issue> reports = finder.check(empty);
         Assertions.assertEquals(0, reports.size());
     }
 
     @Test
-    public void testMiddleMan() {
+    public void testMiddleMan() throws IOException, ParsingException {
+        File f = new File("./src/test/fixtures/smells/middleMan.json");
+        Program program = ProgramParser.parseProgram(f.getName(), mapper.readTree(f));
+
         MiddleMan finder = new MiddleMan();
-        Set<Issue> reports = finder.check(middleMan);
+        Set<Issue> reports = finder.check(program);
         Assertions.assertEquals(3, reports.size());
     }
 }
