c431c0cabfd184c229589870dbff8e6efb240aa4
Florian Obermueller <obermuel@fim.uni-passau.de>
2020-12-21 14:16:01 +0100

    add new customisation to Comparing Literals checking for two variable when there are two String literals

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/hint/ComparingLiteralsHintFactory.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/hint/ComparingLiteralsHintFactory.java
@@ -8,11 +8,11 @@ import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import de.uni_passau.fim.se2.litterbox.ast.model.Script;
 import de.uni_passau.fim.se2.litterbox.ast.model.expression.bool.Not;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.StringLiteral;
 import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinition;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.common.WaitUntil;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.IfElseStmt;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.IfThenStmt;
-import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.RepeatForeverStmt;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.UntilStmt;
 import de.uni_passau.fim.se2.litterbox.utils.IssueTranslator;
 
@@ -26,39 +26,45 @@ public abstract class ComparingLiteralsHintFactory {
     public static final String DEFAULT_FALSE = "comparing_literals_default_false";
     public static final String DEFAULT_VARIABLE = "comparing_literals_default_variable";
     public static final String DEFAULT_VARIABLE_EXISTS = "comparing_literals_default_variable_exists";
+    public static final String DEFAULT_VARIABLE_TWO = "comparing_literals_default_variable_two";
+    public static final String DEFAULT_VARIABLE_EXISTS_TWO = "comparing_literals_default_variable_exists_two";
     public static final String DEFAULT_WITHOUT_INFORMATION = "comparing_literals_default_without_information";
     public static final String DEFAULT_VARIABLE_WITHOUT_INFORMATION = "comparing_literals_default_variable_without_information";
     public static final String DEFAULT_VARIABLE_EXISTS_WITHOUT_INFORMATION = "comparing_literals_default_variable_exists_without_information";
+    public static final String DEFAULT_VARIABLE_TWO_WITHOUT_INFORMATION = "comparing_literals_default_variable_two_without_information";
+    public static final String DEFAULT_VARIABLE_EXISTS_TWO_WITHOUT_INFORMATION = "comparing_literals_default_variable_exists_two_without_information";
     public static final String WAIT_TRUE = "comparing_literals_wait_true";
     public static final String WAIT_FALSE = "comparing_literals_wait_false";
     public static final String WAIT_VARIABLE = "comparing_literals_wait_variable";
     public static final String WAIT_VARIABLE_EXISTS = "comparing_literals_wait_variable_exists";
+    public static final String WAIT_VARIABLE_TWO = "comparing_literals_wait_variable_two";
+    public static final String WAIT_VARIABLE_EXISTS_TWO = "comparing_literals_wait_variable_exists_two";
     public static final String HINT_TRUE_FALSE = "TRUEFALSE";
     public static final String ALWAYS_NEVER = "ALWAYSNEVER";
     public static final String ALWAYS = "always";
     public static final String NEVER = "never";
 
-    public static Hint generateHint(boolean value, boolean wait, ASTNode parent, boolean variable, ASTNode currentNode, ActorDefinition currentActor, Program program, ProcedureDefinition currentProcedure, Script currentScript) {
+    public static Hint generateHint(boolean value, boolean wait, ASTNode parent, String variable, String variable2, ASTNode currentNode, ActorDefinition currentActor, Program program, ProcedureDefinition currentProcedure, Script currentScript) {
         Hint hint;
         boolean variableExits = false;
         boolean top = checkIfTopCond(parent);
         boolean topNot = checkIfNotIsTop(parent);
-        if (variable) {
+        if (variable != null) {
             variableExits = checkForVariableAsLiteral(currentNode, program, currentActor, currentProcedure, currentScript);
         }
 
         if (top || topNot) {
             if (wait) {
                 if (variableExits) {
-                    hint = getWaitHint(value, variable, WAIT_VARIABLE_EXISTS, WAIT_TRUE, WAIT_FALSE);
+                    hint = getWaitHint(value, variable, variable2, WAIT_VARIABLE_EXISTS, WAIT_VARIABLE_EXISTS_TWO, WAIT_TRUE, WAIT_FALSE);
                 } else {
-                    hint = getWaitHint(value, variable, WAIT_VARIABLE, WAIT_TRUE, WAIT_FALSE);
+                    hint = getWaitHint(value, variable, variable2, WAIT_VARIABLE, WAIT_VARIABLE_TWO, WAIT_TRUE, WAIT_FALSE);
                 }
             } else {
                 if (variableExits) {
-                    hint = getWaitHint(value, variable, DEFAULT_VARIABLE_EXISTS, DEFAULT_TRUE, DEFAULT_FALSE);
+                    hint = getWaitHint(value, variable, variable2, DEFAULT_VARIABLE_EXISTS, DEFAULT_VARIABLE_EXISTS_TWO, DEFAULT_TRUE, DEFAULT_FALSE);
                 } else {
-                    hint = getWaitHint(value, variable, DEFAULT_VARIABLE, DEFAULT_TRUE, DEFAULT_FALSE);
+                    hint = getWaitHint(value, variable, variable2, DEFAULT_VARIABLE, DEFAULT_VARIABLE_EXISTS_TWO, DEFAULT_TRUE, DEFAULT_FALSE);
                 }
             }
             if (top) {
@@ -73,14 +79,23 @@ public abstract class ComparingLiteralsHintFactory {
                 } else {
                     hint.setParameter(ALWAYS_NEVER, IssueTranslator.getInstance().getInfo(NEVER));
                 }
-
             }
             setBlock(parent, hint, top);
         } else {
-            if (variable && variableExits) {
+            if(variable != null && variable2 != null && variableExits){
+                hint = new Hint(DEFAULT_VARIABLE_EXISTS_TWO_WITHOUT_INFORMATION);
+                hint.setParameter(Hint.HINT_VARIABLE1, variable);
+                hint.setParameter(Hint.HINT_VARIABLE2, variable2);
+            }else if (variable != null && variable2 != null){
+                hint = new Hint(DEFAULT_VARIABLE_TWO_WITHOUT_INFORMATION);
+                hint.setParameter(Hint.HINT_VARIABLE1, variable);
+                hint.setParameter(Hint.HINT_VARIABLE2, variable2);
+            } else if (variable != null && variableExits) {
                 hint = new Hint(DEFAULT_VARIABLE_EXISTS_WITHOUT_INFORMATION);
-            } else if (variable) {
+                hint.setParameter(Hint.HINT_VARIABLE, variable);
+            } else if (variable != null) {
                 hint = new Hint(DEFAULT_VARIABLE_WITHOUT_INFORMATION);
+                hint.setParameter(Hint.HINT_VARIABLE, variable);
             } else {
                 hint = new Hint(DEFAULT_WITHOUT_INFORMATION);
             }
@@ -104,11 +119,17 @@ public abstract class ComparingLiteralsHintFactory {
         }
     }
 
-    private static Hint getWaitHint(boolean value, boolean variable, String waitVariable, String waitTrue, String waitFalse) {
+    private static Hint getWaitHint(boolean value, String variable,String variable2, String waitVariable, String waitVariable2, String waitTrue, String waitFalse) {
         Hint hint;
-        if (variable) {
+        if (variable!= null && variable2 != null){
+            hint = new Hint(waitVariable2);
+            hint.setParameter(HINT_TRUE_FALSE, IssueTranslator.getInstance().getInfo(String.valueOf(value)));
+            hint.setParameter(Hint.HINT_VARIABLE1, variable);
+            hint.setParameter(Hint.HINT_VARIABLE2, variable2);
+        } else if (variable != null) {
             hint = new Hint(waitVariable);
             hint.setParameter(HINT_TRUE_FALSE, IssueTranslator.getInstance().getInfo(String.valueOf(value)));
+            hint.setParameter(Hint.HINT_VARIABLE, variable);
         } else {
             if (value) {
                 hint = new Hint(waitTrue);
@@ -144,12 +165,18 @@ public abstract class ComparingLiteralsHintFactory {
         keys.add(DEFAULT_TRUE);
         keys.add(DEFAULT_VARIABLE);
         keys.add(DEFAULT_VARIABLE_EXISTS);
+        keys.add(DEFAULT_VARIABLE_TWO);
+        keys.add(DEFAULT_VARIABLE_EXISTS_TWO);
         keys.add(WAIT_FALSE);
         keys.add(WAIT_TRUE);
         keys.add(WAIT_VARIABLE);
         keys.add(WAIT_VARIABLE_EXISTS);
+        keys.add(WAIT_VARIABLE_TWO);
+        keys.add(WAIT_VARIABLE_EXISTS_TWO);
         keys.add(DEFAULT_VARIABLE_EXISTS_WITHOUT_INFORMATION);
         keys.add(DEFAULT_VARIABLE_WITHOUT_INFORMATION);
+        keys.add(DEFAULT_VARIABLE_EXISTS_TWO_WITHOUT_INFORMATION);
+        keys.add(DEFAULT_VARIABLE_TWO_WITHOUT_INFORMATION);
         keys.add(DEFAULT_WITHOUT_INFORMATION);
         return keys;
     }
