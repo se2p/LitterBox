530514474a52b0ed844a6d21e42f03ae6a06706c
UssEva <40119154+UssEva@users.noreply.github.com>
2021-04-14 20:39:36 +0200

    Add MergeDoubleEvent refactoring

---
+++ src/main/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/MergeDoubleEvent.java
@@ -0,0 +1,34 @@
+package de.uni_passau.fim.se2.litterbox.refactor.refactorings;
+
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.Script;
+import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
+import de.uni_passau.fim.se2.litterbox.ast.model.event.Event;
+
+public class MergeDoubleEvent implements Refactoring {
+
+    private final Event event1;
+    private final Event event2;
+    private static final String NAME = "merge_double_event";
+
+    public MergeDoubleEvent(Event event1, Event event2) {
+        this.event1 = event1;
+        this.event2 = event2;
+    }
+
+    @Override
+    public Program apply(Program program) {
+        StmtList stmts;
+        if (event2.hasChildren()) {
+            stmts = (StmtList) event2.getChildren();
+            ((Script) event1.getParentNode()).getStmtList().getStmts().addAll(stmts.getStmts());
+        }
+        event2.getParentNode().getChildren().remove(event2);
+        return program;
+    }
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+}
