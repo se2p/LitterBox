3ea3ccd7e49a73a1306d76d14dca17efb059708f
UssEva <40119154+UssEva@users.noreply.github.com>
2021-04-26 08:20:43 +0200

    Add MergeDoubleEventTest and update refactoring

--- src/main/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/MergeDoubleEvent.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/MergeDoubleEvent.java
@@ -1,30 +1,35 @@
 package de.uni_passau.fim.se2.litterbox.refactor.refactorings;
 
-import de.uni_passau.fim.se2.litterbox.ast.model.Program;
-import de.uni_passau.fim.se2.litterbox.ast.model.Script;
-import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
+import de.uni_passau.fim.se2.litterbox.ast.model.*;
 import de.uni_passau.fim.se2.litterbox.ast.model.event.Event;
+import de.uni_passau.fim.se2.litterbox.utils.Preconditions;
 
 public class MergeDoubleEvent implements Refactoring {
 
     private final Event event1;
     private final Event event2;
+    private final Script script1;
+    private final Script script2;
+    private final ScriptList scriptList;
     private static final String NAME = "merge_double_event";
 
     public MergeDoubleEvent(Event event1, Event event2) {
-        this.event1 = event1;
-        this.event2 = event2;
+        this.event1 = Preconditions.checkNotNull(event1);
+        this.event2 = Preconditions.checkNotNull(event2);
+        this.script1 = (Script) event1.getParentNode();
+        this.script2 = (Script) event2.getParentNode();
+        this.scriptList = (ScriptList) script2.getParentNode();
     }
 
     @Override
     public Program apply(Program program) {
         Program refactored = program.deepCopy();
         StmtList stmts;
-        if (event2.hasChildren()) {
-            stmts = (StmtList) event2.getChildren();
-            ((Script) event1.getParentNode()).getStmtList().getStmts().addAll(stmts.getStmts());
+        if (script2.getStmtList().hasStatements()) {
+            stmts = script2.getStmtList();
+            script1.getStmtList().getStmts().addAll(stmts.getStmts());
         }
-        event2.getParentNode().getChildren().remove(event2);
+        scriptList.getScriptList().remove(script2);
         return refactored;
     }
 
---
+++ src/test/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/MergeDoubleEventTest.java
@@ -0,0 +1,88 @@
+package de.uni_passau.fim.se2.litterbox.refactor.refactorings;
+
+import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.Script;
+import de.uni_passau.fim.se2.litterbox.ast.model.ScriptList;
+import de.uni_passau.fim.se2.litterbox.ast.model.event.Event;
+import de.uni_passau.fim.se2.litterbox.ast.model.event.GreenFlag;
+import de.uni_passau.fim.se2.litterbox.ast.parser.Scratch3Parser;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MergeDoubleEventTest {
+
+    @Test
+    public void applyTest() {
+        File testFile = new File("src/test/testprojects/testdoublestmts.sb3");
+        Program program = null;
+        try {
+            program = new Scratch3Parser().parseFile(testFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertNotNull(program);
+
+        ActorDefinition actorDefinition = program.getActorDefinitionList().getDefinitions().get(1);
+        List<Script> scriptList = actorDefinition.getScripts().getScriptList();
+        Event event1 = null;
+        Event event2 = null;
+        for (Script script : scriptList) {
+            if (event1 == null) {
+                event1 = script.getEvent();
+            } else {
+                event2 = script.getEvent();
+            }
+        }
+        assertNotNull(event1);
+        assertNotNull(event2);
+        assertEquals(event1, event2);
+
+        MergeDoubleEvent refactoring = new MergeDoubleEvent(event2, event1);
+        refactoring.apply(program);
+
+        Event refactoredEvent = null;
+        for (Script script : scriptList) {
+            if (script.getEvent() != null) {
+                refactoredEvent = script.getEvent();
+                break;
+            }
+        }
+        assertNotNull(refactoredEvent);
+        assertEquals(event2, refactoredEvent);
+    }
+
+    @Test
+    public void getNameTest() {
+        GreenFlag greenFlag = mock(GreenFlag.class);
+        Script script = mock(Script.class);
+        ScriptList scriptList = mock(ScriptList.class);
+
+        when(greenFlag.getParentNode()).thenReturn(script);
+        when(script.getParentNode()).thenReturn(scriptList);
+
+        MergeDoubleEvent refactoring = new MergeDoubleEvent(greenFlag, greenFlag);
+        assertEquals("merge_double_event", refactoring.getName());
+    }
+
+    @Test
+    public void toStringTest() {
+        GreenFlag greenFlag = mock(GreenFlag.class);
+        Script script = mock(Script.class);
+        ScriptList scriptList = mock(ScriptList.class);
+
+        when(greenFlag.getParentNode()).thenReturn(script);
+        when(script.getParentNode()).thenReturn(scriptList);
+        when(greenFlag.getUniqueName()).thenReturn("Event");
+
+        MergeDoubleEvent refactoring = new MergeDoubleEvent(greenFlag, greenFlag);
+        assertEquals("merge_double_event(Event, Event)", refactoring.toString());
+    }
+}
