bbf284d9771a35801cf5b9b72a2b3cfa8b4d5b17
Lena Bloch <blochl@fim.uni-passau.de>
2021-05-03 14:36:44 +0200

    add GlidingMotionTest

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/goodpractices/GlidingMotion.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/goodpractices/GlidingMotion.java
@@ -3,9 +3,14 @@ package de.uni_passau.fim.se2.litterbox.analytics.goodpractices;
 import de.uni_passau.fim.se2.litterbox.analytics.AbstractIssueFinder;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueSeverity;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueType;
+import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 import de.uni_passau.fim.se2.litterbox.ast.model.Script;
 import de.uni_passau.fim.se2.litterbox.ast.model.event.KeyPressed;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritemotion.GlideSecsTo;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritemotion.GlideSecsToXY;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Gliding Motion means after a key press the sprite glides to a certain position.
@@ -24,11 +29,17 @@ public class GlidingMotion extends AbstractIssueFinder {
             super.visit(node);
             keyPressed = false;
         }
-        visitChildren(node);
     }
 
     @Override
     public void visit(GlideSecsToXY node) {
+        if (keyPressed) {
+            addIssue(node, node.getMetadata(), IssueSeverity.MEDIUM);
+        }
+    }
+
+    @Override
+    public void visit(GlideSecsTo node) {
         if (keyPressed) {
             addIssue(node, node.getMetadata(), IssueSeverity.MEDIUM);
         }
