42f730819acc44c2c0281c6b2a64310bff943a38
Felix Adler <felix-adler@gmx.de>
2021-05-29 13:07:23 +0200

    Adding test class Adding test json Adding first lines of code

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/refactorings/ExtractEventHandlerFinder.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/refactorings/ExtractEventHandlerFinder.java
@@ -1,4 +1,12 @@
 package de.uni_passau.fim.se2.litterbox.analytics.refactorings;
 
-public class ExtractEventHandlerFinder {
+import de.uni_passau.fim.se2.litterbox.analytics.AbstractRefactoringFinder;
+import de.uni_passau.fim.se2.litterbox.refactor.refactorings.ExtractEventHandler;
+import de.uni_passau.fim.se2.litterbox.refactor.refactorings.MergeEventHandler;
+
+public class ExtractEventHandlerFinder extends AbstractRefactoringFinder {
+    @Override
+    public String getName() {
+        return ExtractEventHandler.NAME;
+    }
 }
