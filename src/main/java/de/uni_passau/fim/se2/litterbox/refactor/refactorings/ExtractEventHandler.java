b5012efe5aed2a5e20b5e65dc38898200f515984
Felix Adler <felix-adler@gmx.de>
2021-06-02 19:41:09 +0200

    First implementation of extract events

--- src/main/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/ExtractEventHandler.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/refactor/refactorings/ExtractEventHandler.java
@@ -1,10 +1,22 @@
 package de.uni_passau.fim.se2.litterbox.refactor.refactorings;
 
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.IfThenStmt;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.RepeatForeverStmt;
 
 public class ExtractEventHandler implements Refactoring{
 
     public static final String NAME = "extract_event_handler";
+
+    private RepeatForeverStmt loop;
+    private IfThenStmt ifThenStmt;
+
+    public ExtractEventHandler(RepeatForeverStmt loop, IfThenStmt ifThenStmt) {
+        this.loop = loop;
+        this.ifThenStmt = ifThenStmt;
+
+        // Check Precoditions
+    }
 
     @Override
     public Program apply(Program program) {
