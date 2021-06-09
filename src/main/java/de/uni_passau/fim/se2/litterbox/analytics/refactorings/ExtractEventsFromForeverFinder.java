64809c1f5cce6dddd3930e78c5a6b7b87e5c1f61
Gordon Fraser <gordon.fraser@uni-passau.de>
2021-06-09 10:41:33 +0200

    Restrict to scripts that contain only a forever loop and no other statements

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/refactorings/ExtractEventsFromForeverFinder.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/refactorings/ExtractEventsFromForeverFinder.java
@@ -2,6 +2,8 @@ package de.uni_passau.fim.se2.litterbox.analytics.refactorings;
 
 import de.uni_passau.fim.se2.litterbox.analytics.AbstractRefactoringFinder;
 import de.uni_passau.fim.se2.litterbox.ast.model.Script;
+import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
+import de.uni_passau.fim.se2.litterbox.ast.model.event.Never;
 import de.uni_passau.fim.se2.litterbox.ast.model.expression.bool.BoolExpr;
 import de.uni_passau.fim.se2.litterbox.ast.model.expression.bool.IsKeyPressed;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.Stmt;
@@ -30,24 +32,26 @@ public class ExtractEventsFromForeverFinder extends AbstractRefactoringFinder {
 
     @Override
     public void visit(Script script) {
-        for (Stmt stmt : script.getStmtList().getStmts()) {
-            // Check if there is a loop at the top level.
-            if(stmt instanceof RepeatForeverStmt) {
-                boolean isValid = false;
-                RepeatForeverStmt repeatForeverStmt = (RepeatForeverStmt) stmt;
-                for(Stmt stmtForever : repeatForeverStmt.getStmtList().getStmts()) {
-                    // Check for ifThen statement.
-                    if( stmtForever instanceof IfThenStmt) {
-                        // Check the bool expression for isKeyPressed event.
-                        IfThenStmt ifThenStmt = (IfThenStmt) stmtForever;
-                        BoolExpr expr = ifThenStmt.getBoolExpr();
-                        if(expr instanceof IsKeyPressed) {
-                            isValid = true;
-                        }
-                    }
-                }
-                if(isValid) {
+        if (script.getEvent() instanceof Never) {
+            return; // Ignore loose blocks
+        }
+
+        StmtList statements = script.getStmtList();
+        if (statements.getNumberOfStatements() != 1 || !(statements.getStatement(0) instanceof RepeatForeverStmt)) {
+            return; // Only scripts consisting only of the loop
+        }
+
+        RepeatForeverStmt repeatForeverStmt = (RepeatForeverStmt) statements.getStatement(0);
+
+        for(Stmt stmtForever : repeatForeverStmt.getStmtList().getStmts()) {
+            // Check for ifThen statement.
+            if( stmtForever instanceof IfThenStmt) {
+                // Check the bool expression for isKeyPressed event.
+                IfThenStmt ifThenStmt = (IfThenStmt) stmtForever;
+                BoolExpr expr = ifThenStmt.getBoolExpr();
+                if(expr instanceof IsKeyPressed) {
                     refactorings.add(new ExtractEventsFromForever(script, repeatForeverStmt));
+                    return;
                 }
             }
         }
