a9327002f5a3b97151ba16938c80661e263b68d1
Gordon Fraser <gordon.fraser@uni-passau.de>
2021-06-10 23:32:18 +0200

    Remove cache and update interface of fitness function

--- src/main/java/de/uni_passau/fim/se2/litterbox/refactor/metaheuristics/fitness_functions/CategoryEntropyFitness.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/refactor/metaheuristics/fitness_functions/CategoryEntropyFitness.java
@@ -4,32 +4,16 @@ import de.uni_passau.fim.se2.litterbox.analytics.metric.CategoryEntropy;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import de.uni_passau.fim.se2.litterbox.refactor.metaheuristics.chromosomes.RefactorSequence;
 
-import java.util.LinkedHashMap;
-
 public class CategoryEntropyFitness implements MinimizingFitnessFunction<RefactorSequence>{
 
-    private Program program;
-    private LinkedHashMap<RefactorSequence, Double> cache = new LinkedHashMap<>();
     private static final String NAME = "category_entropy_fitness";
 
-    public CategoryEntropyFitness(Program program) {
-        this.program = program;
-    }
-
     @Override
-    public double getFitness(RefactorSequence refactorSequence) throws NullPointerException {
-        if(cache.containsKey(refactorSequence)) {
-            return cache.get(refactorSequence);
-        } else {
-            Program refactoredProgram = refactorSequence.getRefactoredProgram();
+    public double getFitness(RefactorSequence refactorSequence) {
+        Program refactoredProgram = refactorSequence.getRefactoredProgram();
 
-            CategoryEntropy entropy = new CategoryEntropy();
-            double fitness = entropy.calculateMetric(refactoredProgram);
-            double normalizedFitness = fitness / (1 + fitness);
-            cache.put(refactorSequence, normalizedFitness);
-
-            return normalizedFitness;
-        }
+        CategoryEntropy entropy = new CategoryEntropy();
+        return entropy.calculateMetric(refactoredProgram);
     }
 
     @Override
