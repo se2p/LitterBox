e01162921e3f9736585769f1d31795fd4f00a867
labrenz <simon.labrenz@outlook.de>
2021-06-18 12:34:52 +0200

    Add NumberOfBlock Fitness Function again.

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/RefactoringAnalyzer.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/RefactoringAnalyzer.java
@@ -130,7 +130,7 @@ public class RefactoringAnalyzer extends Analyzer {
         fitnessFunctions.add(new AverageEntropyFitness());
 
         // fitnessFunctions.add(new HalsteadDifficultyFitness());
-        // fitnessFunctions.add(new NumberOfBlocksFitness(program));
+        fitnessFunctions.add(new NumberOfBlocksFitness());
         // fitnessFunctions.add(new CategoryEntropyFitness());
         FastNonDominatedSort<RefactorSequence> fastNonDominatedSort = new FastNonDominatedSort<>(fitnessFunctions);
         CrowdingDistanceSort<RefactorSequence> crowdingDistanceSort = new CrowdingDistanceSort<>(fitnessFunctions);
