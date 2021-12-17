cb98e37e171fc3fc7ac271f953fa83445ef46516
Florian Beck <florianbeck.1997@gmx.de>
2021-12-17 19:00:48 +0100

    added getTokens()

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/code2vec/PathGenerator.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/code2vec/PathGenerator.java
@@ -3,7 +3,23 @@ package de.uni_passau.fim.se2.litterbox.analytics.code2vec;
 import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.event.EventAttribute;
+import de.uni_passau.fim.se2.litterbox.ast.model.expression.num.NumFunct;
+import de.uni_passau.fim.se2.litterbox.ast.model.expression.string.NameNum;
+import de.uni_passau.fim.se2.litterbox.ast.model.expression.string.attributes.FixedAttribute;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.BoolLiteral;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.NumberLiteral;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.StringLiteral;
+import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.ProcedureMutationMetadata;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.actorlook.GraphicEffect;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.actorsound.SoundEffect;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritelook.ForwardBackwardChoice;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritelook.LayerChoice;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritemotion.DragMode;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.spritemotion.RotationStyle;
+import de.uni_passau.fim.se2.litterbox.ast.model.timecomp.TimeComp;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ExtractSpriteVisitor;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,7 +59,7 @@ public class PathGenerator {
                     ": " + leafsMap.get(key).size());
             int i = 0;
             for (ASTNode value : leafsMap.get(key)) {
-                System.out.println(i + "Blatt (Test): " + value.getUniqueName());
+                System.out.println(i + "Blatt (Test): " + getToken(value));
                 i++;
             }
         }
@@ -80,8 +96,11 @@ public class PathGenerator {
             for (int j = i + 1; j < spriteLeafs.size(); j++) {
                 String path = generatePath(spriteLeafs.get(i), spriteLeafs.get(j));
                 if (!path.isEmpty()) {
-                    String source = spriteLeafs.get(i).getUniqueName();
-                    String target = spriteLeafs.get(j).getUniqueName();
+                    String source = getToken(spriteLeafs.get(i));
+                    String target = getToken(spriteLeafs.get(j));
+                    if (source.isEmpty() || target.isEmpty()) {
+                        return null;
+                    }
                     programFeatures.addFeature(source, path, target);
                 }
             }
@@ -163,5 +182,44 @@ public class PathGenerator {
             }
         }
         return isDefaultName;
+    }
+
+    private String getToken(ASTNode leaf) {
+        String token = leaf.getUniqueName();
+
+        //FIXME: ColorLiteral is still missing
+        if (leaf instanceof StringLiteral) {
+            token = ((StringLiteral) leaf).getText();
+        } else if (leaf instanceof NumberLiteral){
+            token = Double.toString(((NumberLiteral) leaf).getValue());
+        } else if (leaf instanceof BoolLiteral){
+            token = Boolean.toString(((BoolLiteral) leaf).getValue());
+        } else if (leaf instanceof TimeComp) {
+            token = ((TimeComp) leaf).getTypeName();
+        } else if (leaf instanceof SoundEffect) {
+            token = ((SoundEffect) leaf).getTypeName();
+        } else if (leaf instanceof RotationStyle) {
+            token = ((RotationStyle) leaf).getTypeName();
+        } else if (leaf instanceof ProcedureMutationMetadata) {
+            token = Boolean.toString(((ProcedureMutationMetadata) leaf).isWarp());
+        } else if (leaf instanceof NumFunct) {
+            token = ((NumFunct) leaf).getTypeName();
+        } else if (leaf instanceof NameNum) {
+            token = ((NameNum) leaf).getTypeName();
+        } else if (leaf instanceof LayerChoice) {
+            token = ((LayerChoice) leaf).getTypeName();
+        } else if (leaf instanceof GraphicEffect) {
+            token = ((GraphicEffect) leaf).getTypeName();
+        } else if (leaf instanceof ForwardBackwardChoice) {
+            token = ((ForwardBackwardChoice) leaf).getTypeName();
+        } else if (leaf instanceof EventAttribute) {
+            token = ((EventAttribute) leaf).getTypeName();
+        } else if (leaf instanceof DragMode) {
+            token = ((DragMode) leaf).getTypeName();
+        } else if (leaf instanceof FixedAttribute) {
+            token = ((FixedAttribute) leaf).getTypeName();
+        }
+        token = StringUtils.deleteWhitespace(token);
+        return token;
     }
 }
