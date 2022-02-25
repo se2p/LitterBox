3527ba8a2aab187220491d5fe528864bfcb8d826
Gordon Fraser <gordon.fraser@uni-passau.de>
2022-02-25 16:36:15 +0100

    Add duplicate handling for unnecessary ifs

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/smells/UnnecessaryIf.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/smells/UnnecessaryIf.java
@@ -1,13 +1,15 @@
 package de.uni_passau.fim.se2.litterbox.analytics.smells;
 
-import de.uni_passau.fim.se2.litterbox.analytics.AbstractIssueFinder;
-import de.uni_passau.fim.se2.litterbox.analytics.IssueSeverity;
-import de.uni_passau.fim.se2.litterbox.analytics.IssueType;
+import de.uni_passau.fim.se2.litterbox.analytics.*;
+import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 import de.uni_passau.fim.se2.litterbox.ast.model.StmtList;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.Stmt;
 import de.uni_passau.fim.se2.litterbox.ast.model.statement.control.IfThenStmt;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class UnnecessaryIf extends AbstractIssueFinder {
     public static final String NAME = "unnecessary_if";
@@ -17,15 +19,18 @@ public class UnnecessaryIf extends AbstractIssueFinder {
 
         final List<Stmt> stmts = node.getStmts();
         StmtList lastList = null;
+        IfThenStmt lastIf = null;
         for (Stmt s : stmts) {
             if (s instanceof IfThenStmt) {
 
                 if (lastList != null) {
                     if (lastList.equals(((IfThenStmt) s).getThenStmts())) {
-                        addIssue(s, s.getMetadata(), IssueSeverity.LOW);
+                        MultiBlockIssue issue = new MultiBlockIssue(this, IssueSeverity.LOW, program, currentActor, currentScript, Arrays.asList(s, lastIf), s.getMetadata(), new Hint(getName()));
+                        addIssue(issue);
                     }
                 }
                 lastList = ((IfThenStmt) s).getThenStmts();
+                lastIf = (IfThenStmt) s;
             } else {
                 // even if we already have a list from an ifstmt before, it only counts if a second ifstmt
                 // follows directly after the first.
@@ -34,6 +39,42 @@ public class UnnecessaryIf extends AbstractIssueFinder {
         }
 
         visitChildren(node);
+    }
+
+    @Override
+    public boolean isDuplicateOf(Issue first, Issue other) {
+        if (first == other) {
+            // Don't check against self
+            return false;
+        }
+
+        if (first.getFinder() != other.getFinder()) {
+            // Can only be a duplicate if it's the same finder
+            return false;
+        }
+
+        if ((first.getScriptOrProcedureDefinition() == null) || (other.getScriptOrProcedureDefinition() == null)) {
+            // Need to refer to same script
+            return false;
+        }
+
+        if (!first.getScriptOrProcedureDefinition().equals(other.getScriptOrProcedureDefinition())) {
+            // Need to refer to same script
+            return false;
+        }
+
+        if (first instanceof MultiBlockIssue) {
+            MultiBlockIssue mbFirst = (MultiBlockIssue) first;
+            MultiBlockIssue mbOther = (MultiBlockIssue) other;
+            Set<ASTNode> nodes = new HashSet<>(mbFirst.getNodes());
+            nodes.retainAll(mbOther.getNodes());
+            // If there is an overlap, the issues overlap on one if
+            if (!nodes.isEmpty()) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     @Override
--- src/test/java/de/uni_passau/fim/se2/litterbox/analytics/smells/UnnecessaryIfTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox/analytics/smells/UnnecessaryIfTest.java
@@ -1,10 +1,16 @@
 package de.uni_passau.fim.se2.litterbox.analytics.smells;
 
 import de.uni_passau.fim.se2.litterbox.JsonTest;
+import de.uni_passau.fim.se2.litterbox.analytics.Issue;
 import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
 
 public class UnnecessaryIfTest implements JsonTest {
     @Test
@@ -25,5 +31,16 @@ public class UnnecessaryIfTest implements JsonTest {
     @Test
     public void testDifferentIf() throws IOException, ParsingException {
         assertThatFinderReports(0, new UnnecessaryIf(), "./src/test/fixtures/smells/separateIfs.json");
+    }
+
+    @Test
+    public void testDuplicates() throws IOException, ParsingException {
+        UnnecessaryIf unnecessaryIf = new UnnecessaryIf();
+        Program program = getAST("./src/test/fixtures/smells/unnecessaryIfDuplicate.json");
+        List<Issue> issues = new ArrayList<>(unnecessaryIf.check(program));
+        assertThat(issues).hasSize(2);
+        assertThat(issues.get(0).isDuplicateOf(issues.get(1))).isTrue();
+        assertThat(issues.get(1).isDuplicateOf(issues.get(0))).isTrue();
+        assertThat(issues.get(0).isDuplicateOf(issues.get(0))).isFalse();
     }
 }
