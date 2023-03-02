dae9ef45e1e837358e40587a6ed1d7de04f776a7
AlaaEzzeldin <khalila@fim.uni-passau.de>
2023-03-02 21:04:57 +0100

    code2vec_scripts: add testCreateContextForCode2VecPerScripts

--- src/test/java/de/uni_passau/fim/se2/litterbox/analytics/ml_preprocessing/code2vec/GeneratePathTaskTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox/analytics/ml_preprocessing/code2vec/GeneratePathTaskTest.java
@@ -25,7 +25,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,6 +40,11 @@ class GeneratePathTaskTest implements JsonTest {
     final static String CAT_PATHS = "cat 39,625791294,Hi! 39,1493538624,Show Hi!,-547448667,Show";
     final static String ABBY_PATHS = "abby GreenFlag,-2069003229,Hello!";
     final static String STAGE_PATHS = "stage GreenFlag,1809747443,10";
+
+    /**
+     * the only sprite that has a script contains more than one leaf: Cat
+     */
+    final static String CAT_SCRIPT_PATH = "Hi!,-547448667,Show";
 
     @Test
     void testCreateContextEmptyProgram() throws ParsingException, IOException {
@@ -68,5 +77,31 @@ class GeneratePathTaskTest implements JsonTest {
         } else {
             assertThat(pathContextsForCode2Vec).doesNotContain(STAGE_PATHS);
         }
+    }
+
+    @ParameterizedTest(name = "{displayName} [{index}] includeStage={0}")
+    @ValueSource(booleans = {true, false})
+    void testCreateContextForCode2VecPerScripts(boolean includeStage) throws ParsingException, IOException {
+        Program program = getAST("src/test/fixtures/multipleSprites.json");
+        PathGenerator pathGenerator = PathGeneratorFactory.createPathGenerator(false, true, 8, includeStage, program);
+        GeneratePathTask generatePathTask = new GeneratePathTask(pathGenerator);
+        List<String> pathContextsForCode2Vec = generatePathTask.createContextForCode2Vec().collect(Collectors.toList());
+
+        // temp sol to remove the Script name TODO clean that up when changing createContextForCode2Vec return type
+        List<String> pathContextsForCode2Vec2 = new ArrayList<>();
+        for (String token : pathContextsForCode2Vec) {
+            int x= token.indexOf(" ");
+            pathContextsForCode2Vec2.add(token.substring(x+1));
+        }
+
+
+        if (includeStage) {
+            assertThat(pathContextsForCode2Vec2).hasSize(1);
+        } else {
+            assertThat(pathContextsForCode2Vec2).hasSize(1);
+        }
+
+        assertThat(pathContextsForCode2Vec2).contains(CAT_SCRIPT_PATH);
+
     }
 }
