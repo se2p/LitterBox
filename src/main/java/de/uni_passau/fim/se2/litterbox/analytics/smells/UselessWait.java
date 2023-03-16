0ec9f8bb5e0349fc8bceeb15c2ab6d37f7c488b0
obermuel <obermuel@fim.uni-passau.de>
2023-03-16 12:39:44 +0100

    scaffolding of finder

---
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/smells/UselessWait.java
@@ -0,0 +1,25 @@
+package de.uni_passau.fim.se2.litterbox.analytics.smells;
+
+import de.uni_passau.fim.se2.litterbox.analytics.AbstractIssueFinder;
+import de.uni_passau.fim.se2.litterbox.analytics.IssueType;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.common.WaitSeconds;
+
+public class UselessWait extends AbstractIssueFinder {
+    public static final String NAME = "useless_wait";
+
+    @Override
+    public void visit(WaitSeconds node){
+
+    }
+
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public IssueType getIssueType() {
+        return IssueType.SMELL;
+    }
+}
