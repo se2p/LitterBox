96a406d1a7ed1f11892b5e22ac83f2e4eb540450
AlaaEzzeldin <khalila@fim.uni-passau.de>
2023-06-06 18:27:08 +0200

    resolve merge conflicts

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Analyzer.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/Analyzer.java
@@ -57,9 +57,7 @@ public abstract class Analyzer {
     public void analyzeFile() throws IOException {
         File file = input.toFile();
         if (file.exists() && file.isDirectory()) {
-            var listOfFiles =  getProgramPaths(String.valueOf(file)) ;
-            for (String filePath : listOfFiles) {
-                var fileEntry = new File(String.valueOf(filePath));
+            for (final File fileEntry : Objects.requireNonNull(file.listFiles())) {
                 if (!fileEntry.isDirectory()) {
                     check(fileEntry, output);
                     deleteFile(fileEntry);
@@ -73,13 +71,6 @@ public abstract class Analyzer {
         }
     }
 
-    private static List<String> getProgramPaths(String dirPath) throws IOException {
-        return Files.walk(Paths.get(dirPath))
-                .filter(p -> !Files.isDirectory(p))
-                .map(Path::toString)
-                .filter(f -> f.endsWith("json"))
-                .collect(Collectors.toList());
-    }
     private void deleteFile(File file) {
         if (delete && (file.getName().endsWith(".json") || file.getName().endsWith(".sb3"))) {
             boolean success = file.delete();
