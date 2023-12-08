434a358bb5dd082e53cfaad60a16660156d2eae4
Florian Obermüller <obermuel@fim.uni-passau.de>
2023-12-08 11:06:04 +0100

    finish MissingCloneInitializationFix

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/fix_heuristics/MissingCloneInitializationFix.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/fix_heuristics/MissingCloneInitializationFix.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2019-2022 LitterBox contributors
+ *
+ * This file is part of LitterBox.
+ *
+ * LitterBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * LitterBox is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LitterBox. If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.uni_passau.fim.se2.litterbox.analytics.fix_heuristics;
 
 import de.uni_passau.fim.se2.litterbox.analytics.AbstractIssueFinder;
@@ -19,7 +37,7 @@ import java.util.Objects;
 import java.util.Set;
 
 public class MissingCloneInitializationFix extends AbstractIssueFinder {
-    public static final String NAME = "message_never_sent_fix";
+    public static final String NAME = "missing_clone_initialization_fix";
     private final String bugLocationBlockId;
     private boolean firstRun = false;
     private String clonedActor;
