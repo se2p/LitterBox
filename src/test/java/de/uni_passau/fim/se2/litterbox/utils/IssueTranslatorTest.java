90ac5ca8e4ce9f00f65d45274b73410b3a49bdb5
Benedikt Fein <fein@fim.uni-passau.de>
2024-04-03 14:39:06 +0200

    refactor: only load resource bundles in IssueTranslator again if language actually changed

---
+++ src/test/java/de/uni_passau/fim/se2/litterbox/utils/IssueTranslatorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2019-2022 LitterBox contributors
+ *
+ * This file is part of LitterBox.
+ *
+ * LitterBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * LitterBox is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LitterBox. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.uni_passau.fim.se2.litterbox.utils;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Isolated  // ensure no parallel tests to avoid parallel mutation of global state (i.e. IssueTranslator language)
+class IssueTranslatorTest {
+    @Test
+    void fallbackToEnglish() {
+        IssueTranslator.getInstance().setLanguage("no");
+        assertEquals("costume", getInfo(IssueTranslator.GeneralTerm.COSTUME));
+    }
+
+    @Test
+    void fallbackToEnglishMissingLocale() {
+        IssueTranslator.getInstance().setLanguage(null);
+        assertEquals("attribute", getInfo(IssueTranslator.GeneralTerm.ATTRIBUTE));
+    }
+
+    @Test
+    void switchLanguage() {
+        IssueTranslator.getInstance().setLanguage("en");
+        assertEquals("Visibility", getInfo(IssueTranslator.GeneralTerm.VISIBILITY));
+        IssueTranslator.getInstance().setLanguage("de");
+        assertEquals("Sichtbarkeit", getInfo(IssueTranslator.GeneralTerm.VISIBILITY));
+    }
+
+    private String getInfo(final IssueTranslator.GeneralTerm term) {
+        return IssueTranslator.getInstance().getInfo(term);
+    }
+}
