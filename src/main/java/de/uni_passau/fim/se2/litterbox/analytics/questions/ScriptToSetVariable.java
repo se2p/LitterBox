63f0d80245e515c5c0d7df807c97f1e07efae1a1
Emily Courtney <emilycourtney@posteo.net>
2024-07-04 21:53:21 +0200

    Implement ScriptToSetVariable question

---
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/questions/ScriptToSetVariable.java
@@ -0,0 +1,73 @@
+package de.uni_passau.fim.se2.litterbox.analytics.questions;
+
+import de.uni_passau.fim.se2.litterbox.analytics.Hint;
+import de.uni_passau.fim.se2.litterbox.analytics.IssueBuilder;
+import de.uni_passau.fim.se2.litterbox.analytics.IssueSeverity;
+import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.Script;
+import de.uni_passau.fim.se2.litterbox.ast.model.ScriptList;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.common.SetVariableTo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.isNull;
+
+/**
+ * @QuestionType Multiple Choice
+ * @NumAnswers 1
+ * @NumChoices {@code MAX_CHOICES}
+ * @Highlighted Nothing
+ * @Context Whole program
+ */
+public class ScriptToSetVariable extends AbstractQuestionFinder {
+    private String variable;
+    private Map<String, String> answers;
+
+    @Override
+    public void visit(Program node) {
+        answers = new HashMap<>();
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(ScriptList node) {
+        choices.clear();
+        super.visit(node);
+
+        if (!choices.isEmpty() && !answers.isEmpty()) {
+            answers.forEach((variable, answer) -> {
+                IssueBuilder builder = prepareIssueBuilder().withSeverity(IssueSeverity.LOW);
+                Hint hint = new Hint(getName());
+                hint.setParameter(Hint.HINT_VARIABLE, variable);
+                hint.setParameter(Hint.ANSWER, answer);
+                hint.setParameter(Hint.CHOICES, getChoices());
+                addIssue(builder.withHint(hint));
+            });
+        }
+    }
+
+    @Override
+    public void visit(Script node) {
+        variable = null;
+        super.visit(node);
+        if (!isNull(variable)) {
+            answers.putIfAbsent(variable, wrappedScratchBlocks(node));
+        }
+        else {
+            choices.add(wrappedScratchBlocks(node));
+        }
+
+        currentScript = null;
+    }
+
+    @Override
+    public void visit(SetVariableTo node) {
+        variable = node.getIdentifier().getScratchBlocks();
+    }
+
+    @Override
+    public String getName() {
+        return "script_to_set_variable";
+    }
+}
