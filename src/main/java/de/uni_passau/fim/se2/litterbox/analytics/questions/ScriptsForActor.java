896423b46a3c0afaf6b65aa7e385281241da9c59
Emily Courtney <emilycourtney@posteo.net>
2024-07-16 15:49:25 +0200

    Use translations for Stage name in ScriptsForActor

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/questions/ScriptsForActor.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/questions/ScriptsForActor.java
@@ -4,6 +4,7 @@ import de.uni_passau.fim.se2.litterbox.analytics.Hint;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueBuilder;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueSeverity;
 import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
+import de.uni_passau.fim.se2.litterbox.utils.IssueTranslator;
 
 /**
  * @QuestionType Number
@@ -17,7 +18,8 @@ public class ScriptsForActor extends AbstractQuestionFinder {
         currentActor = node;
         IssueBuilder builder = prepareIssueBuilder().withSeverity(IssueSeverity.LOW);
         Hint hint = new Hint(getName());
-        hint.setParameter(Hint.ACTOR, node.getIdent().getScratchBlocksWithoutNewline());
+        String actor = node.getIdent().getScratchBlocks();
+        hint.setParameter(Hint.ACTOR, (actor.equals("Stage") ? IssueTranslator.getInstance().getInfo("stage") : actor));
         hint.setParameter(Hint.ANSWER, Integer.toString(node.getScripts().getSize()));
         addIssue(builder.withHint(hint));
     }
