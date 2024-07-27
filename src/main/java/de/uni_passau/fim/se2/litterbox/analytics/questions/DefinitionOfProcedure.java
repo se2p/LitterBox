e7a14e36176722c26460678c7066723bafc8ddd0
Emily Courtney <emilycourtney@posteo.net>
2024-07-27 12:04:00 +0200

    Change DefinitionOfProcedure question to reference a specific CallStmt

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/questions/DefinitionOfProcedure.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/questions/DefinitionOfProcedure.java
@@ -4,9 +4,9 @@ import de.uni_passau.fim.se2.litterbox.analytics.Hint;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueBuilder;
 import de.uni_passau.fim.se2.litterbox.analytics.IssueSeverity;
 import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinition;
-import de.uni_passau.fim.se2.litterbox.ast.model.ActorDefinitionList;
 import de.uni_passau.fim.se2.litterbox.ast.model.Script;
 import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinition;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.CallStmt;
 
 import java.util.HashMap;
 import java.util.LinkedHashSet;
@@ -15,52 +15,64 @@ import java.util.Set;
 
 public class DefinitionOfProcedure extends AbstractQuestionFinder {
 
-    private Map<ProcedureDefinition, ActorDefinition> procedures;
+    private Map<String, ProcedureDefinition> procedures;
+    private Map<String, CallStmt> procedureCalls;
+    private Map<CallStmt, Script> scriptForCall;
+    private Map<CallStmt, ActorDefinition> actorForCall;
     private Set<Script> scripts;
 
     @Override
-    public void visit(ActorDefinitionList node) {
+    public void visit(ActorDefinition node) {
         procedures = new HashMap<>();
+        procedureCalls = new HashMap<>();
+        scriptForCall = new HashMap<>();
+        actorForCall = new HashMap<>();
         scripts = new LinkedHashSet<>();
 
         super.visit(node);
-        currentScript = null;
-        currentProcedure = null;
 
-        procedures.forEach((procedure, actor) -> {
-            choices.clear();
-            // prioritize other procedure definitions in list of choices
-            procedures.forEach((p, a) -> {
-                if (!p.equals(procedure)) {
-                    currentActor = a;
-                    choices.add(wrappedScratchBlocks(p));
-                }
-            });
-            scripts.forEach(s -> {
-                if (choices.size() < MAX_CHOICES)
-                    choices.add(wrappedScratchBlocks(s));
-            });
+        procedureCalls.forEach((ident, stmt) -> {
+            if (procedures.containsKey(ident)) {
+                choices.clear();
+                // prioritize other procedure definitions in list of choices
+                procedures.forEach((i, p) -> {
+                    if (!i.equals(ident)) {
+                        choices.add(wrappedScratchBlocks(p));
+                    }
+                });
+                scripts.forEach(s -> {
+                    if (choices.size() < MAX_CHOICES)
+                        choices.add(wrappedScratchBlocks(s));
+                });
 
-            currentActor = actor;
-            String procedureName = program.getProcedureMapping().getProcedures().get(currentActor.getIdent().getName()).get(procedure.getIdent()).getName();
+                currentScript = scriptForCall.get(stmt);
+                currentActor = actorForCall.get(stmt);
 
-            IssueBuilder builder = prepareIssueBuilder().withSeverity(IssueSeverity.LOW);
-            Hint hint = new Hint(getName());
-            hint.setParameter(Hint.PROCEDURE, procedureName);
-            hint.setParameter(Hint.CHOICES, getChoices());
-            hint.setParameter(Hint.ANSWER, wrappedScratchBlocks(procedure));
-            addIssue(builder.withHint(hint));
+                IssueBuilder builder = prepareIssueBuilder(stmt).withSeverity(IssueSeverity.LOW);
+                Hint hint = new Hint(getName());
+                hint.setParameter(Hint.CHOICES, getChoices());
+                hint.setParameter(Hint.ANSWER, wrappedScratchBlocks(procedures.get(ident)));
+                addIssue(builder.withHint(hint));
+            }
         });
     }
 
     @Override
+    public void visit(CallStmt node) {
+        procedureCalls.putIfAbsent(node.getIdent().getName(), node);
+        scriptForCall.putIfAbsent(node, currentScript);
+        actorForCall.putIfAbsent(node, currentActor);
+    }
+
+    @Override
     public void visit(ProcedureDefinition node) {
-        super.visit(node);
-        procedures.put(node, currentActor);
+        String procedureName = program.getProcedureMapping().getProcedures().get(currentActor.getIdent().getName()).get(node.getIdent()).getName();
+        procedures.put(procedureName, node);
     }
 
     @Override
     public void visit(Script node) {
+        super.visit(node);
         scripts.add(node);
     }
 
