91cda87241b919b4956c1ef164ef90e75e10694c
Benedikt Fein <fein@fim.uni-passau.de>
2024-09-24 18:49:43 +0200

    fix(parser): do not parse variables with wrong shadow as literal
    Fixes #494

--- src/test/java/de/uni_passau/fim/se2/litterbox/ast/new_parser/NewParserRegressionTest.java
+++ src/test/java/de/uni_passau/fim/se2/litterbox/ast/new_parser/NewParserRegressionTest.java
@@ -3,12 +3,14 @@ package de.uni_passau.fim.se2.litterbox.ast.new_parser;
 import de.uni_passau.fim.se2.litterbox.JsonTest;
 import de.uni_passau.fim.se2.litterbox.ast.ParsingException;
 import de.uni_passau.fim.se2.litterbox.ast.model.Program;
+import de.uni_passau.fim.se2.litterbox.ast.model.expression.bool.Equals;
 import de.uni_passau.fim.se2.litterbox.ast.model.extensions.pen.SetPenColorToColorStmt;
 import de.uni_passau.fim.se2.litterbox.ast.model.extensions.texttospeech.Speak;
 import de.uni_passau.fim.se2.litterbox.ast.model.identifier.Qualified;
 import de.uni_passau.fim.se2.litterbox.ast.model.literals.ColorLiteral;
 import de.uni_passau.fim.se2.litterbox.ast.model.touchable.color.FromNumber;
 import de.uni_passau.fim.se2.litterbox.ast.model.variable.ScratchList;
+import de.uni_passau.fim.se2.litterbox.ast.model.variable.Variable;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.NodeFilteringVisitor;
 import org.junit.jupiter.api.Test;
 
@@ -37,5 +39,15 @@ class NewParserRegressionTest implements JsonTest {
         final Qualified speakText = (Qualified) speakBlock.getText().getChildren().get(0);
 
         assertInstanceOf(ScratchList.class, speakText.getSecond());
+    }
+
+    @Test
+    void acceptVariableNoShadowInput() throws ParsingException, IOException {
+        final Program p = getAST("src/test/fixtures/parserRegressions/acceptVariableInputsHackedShadow.json");
+
+        final var equalsBlock = NodeFilteringVisitor.getBlocks(p, Equals.class).get(0);
+        final Qualified leftOperand = (Qualified) equalsBlock.getOperand1();
+
+        assertInstanceOf(Variable.class, leftOperand.getSecond());
     }
 }
