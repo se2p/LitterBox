b37924192555d17fb5a950638a4f1470a3392f46
Benedikt Fein <fein@fim.uni-passau.de>
2025-01-02 17:06:44 +0100

    scratchblocks: make parser interruptible
    This seems to provide a working cancellation in most situations. Only in
    some cases (e.g. ANTLR performs an expensive equals-check on some deeply
    nested internal parse trees) the cancellation might not work reliably.

--- src/main/java/de/uni_passau/fim/se2/litterbox/scratchblocks/ScratchBlocksParser.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/scratchblocks/ScratchBlocksParser.java
@@ -21,16 +21,46 @@ package de.uni_passau.fim.se2.litterbox.scratchblocks;
 import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 import de.uni_passau.fim.se2.litterbox.ast.model.ScriptEntity;
 import de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksLexer;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.misc.ParseCancellationException;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * A parser of ScratchBlocks code into a LitterBox AST.
+ *
+ * <p>The parser regularly checks for {@link Thread#interrupted()} to abort the parsing early. However, this will not
+ * immediately abort the parsing. Even though it is checked regularly inside the parsing state machine to achieve a
+ * best-effort cancellation, there may be situations where the parser spends a long time in deeper internal states in
+ * which the marker is only rarely checked.
+ * Interrupted parsers throw a {@link ParseCancellationException}.
+ *
+ * @see <a href="https://en.scratch-wiki.info/wiki/Block_Plugin">ScratchBlocks syntax</a>
+ */
 public class ScratchBlocksParser {
 
     // todo: probably similar methods for whole actors and programs?
-
     public ScriptEntity parseScript(final String scratchBlocksCode) {
-        final de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser parser = buildParser(scratchBlocksCode);
+        return parseScript(scratchBlocksCode, new AtomicBoolean(false));
+    }
+
+    /**
+     * Parses the given script in ScratchBlocks code into an AST.
+     *
+     * <p>The {@code cancelMarker} is checked in addition to {@link Thread#interrupted()}. The same limitations as
+     * described in {@link ScratchBlocksParser} apply.
+     *
+     * @param scratchBlocksCode Some ScratchBlocks code.
+     * @param cancelMarker Will try to interrupt the parsing when this marker gets set to {@code true}.
+     * @return The parsed script or custom procedure definition.
+     */
+    public ScriptEntity parseScript(final String scratchBlocksCode, final AtomicBoolean cancelMarker) {
+        final de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser parser = buildParser(
+                cancelMarker, scratchBlocksCode
+        );
         final ParseTree tree = parser.script();
 
         final ScratchBlocksToScratchVisitor vis = new ScratchBlocksToScratchVisitor();
@@ -38,6 +68,8 @@ public class ScratchBlocksParser {
 
         if (node instanceof ScriptEntity script) {
             return script;
+        } else if (node == null) {
+            return null;
         } else {
             throw new IllegalArgumentException(
                     "Could not parse ScratchBlocks code as script. Got: " + node.getClass().getSimpleName()
@@ -45,9 +77,47 @@ public class ScratchBlocksParser {
         }
     }
 
-    private de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser buildParser(final String scratchBlocks) {
+    private de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser buildParser(
+            final AtomicBoolean cancelMarker, final String scratchBlocks
+    ) {
         final ScratchBlocksLexer lexer = new ScratchBlocksLexer(CharStreams.fromString(scratchBlocks));
         final CommonTokenStream tokens = new CommonTokenStream(lexer);
-        return new de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser(tokens);
+        return new ScratchBlocksInterruptibleParser(tokens, cancelMarker);
+    }
+
+    private static final class ScratchBlocksInterruptibleParser
+            extends de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksParser {
+
+        public ScratchBlocksInterruptibleParser(final TokenStream input, final AtomicBoolean cancelMarker) {
+            super(input);
+            _interp = new InterruptibleParserATNSimulator(
+                    this, _ATN, _decisionToDFA, _sharedContextCache, cancelMarker
+            );
+        }
+    }
+
+    private static final class InterruptibleParserATNSimulator extends ParserATNSimulator {
+
+        private final AtomicBoolean cancelMarker;
+
+        public InterruptibleParserATNSimulator(
+                final Parser parser,
+                final ATN atn,
+                final DFA[] decisionToDFA,
+                final PredictionContextCache sharedContextCache,
+                final AtomicBoolean cancelMarker
+        ) {
+            super(parser, atn, decisionToDFA, sharedContextCache);
+            this.cancelMarker = cancelMarker;
+        }
+
+        @Override
+        public PredictionContext getCachedContext(final PredictionContext context) {
+            if (cancelMarker.get() || Thread.interrupted()) {
+                throw new ParseCancellationException("Parsing interrupted.");
+            }
+
+            return super.getCachedContext(context);
+        }
     }
 }
