856b193f419341c372113a47f12ff2e6cd891400
Florian Obermüller <obermuel@fim.uni-passau.de>
2025-01-09 12:14:22 +0100

    add missing finders to tool

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/IssueTool.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/IssueTool.java
@@ -250,11 +250,11 @@ public class IssueTool {
 
         if (LOAD_GENERAL) {
             registerQuestionFinder(new BlockControllingLoop(), questionFinders);
-//            registerQuestionFinder(new DefinitionOfProcedure(), questionFinders);
+            registerQuestionFinder(new DefinitionOfProcedure(), questionFinders);
             registerQuestionFinder(new ElementInLoopBody(), questionFinders);
             registerQuestionFinder(new ElementInLoopCondition(), questionFinders);
             registerQuestionFinder(new IfBlockCondition(), questionFinders);
-//            registerQuestionFinder(new IfElseStatementExecution(), questionFinders);
+            registerQuestionFinder(new IfElseStatementExecution(), questionFinders);
             registerQuestionFinder(new IfThenStatementExecution(), questionFinders);
             registerQuestionFinder(new PurposeOfBroadcast(), questionFinders);
             registerQuestionFinder(new PurposeOfForeverLoop(), questionFinders);
@@ -270,10 +270,10 @@ public class IssueTool {
             registerQuestionFinder(new ScriptsForActor(), questionFinders);
             registerQuestionFinder(new ScriptsInProgram(), questionFinders);
             registerQuestionFinder(new ScriptsTriggeredByEvent(), questionFinders);
-//            registerQuestionFinder(new ScriptsTriggeredByStatement(), questionFinders);
+           registerQuestionFinder(new ScriptsTriggeredByStatement(), questionFinders);
             registerQuestionFinder(new ScriptExecutionOrderDifferentActors(), questionFinders);
             registerQuestionFinder(new ScriptExecutionOrderSameActor(), questionFinders);
-//            registerQuestionFinder(new ScriptToSetVariable(), questionFinders);
+           registerQuestionFinder(new ScriptToSetVariable(), questionFinders);
             registerQuestionFinder(new SetVariable(), questionFinders);
             registerQuestionFinder(new StatementsInIfStatement(), questionFinders);
             registerQuestionFinder(new StatementTriggersEvent(), questionFinders);
