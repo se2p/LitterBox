e8e69c5495d28d1501b9c87691ae0c2348aa0eb7
Jonas Piehler <piehlerj@fim.uni-passau.de>
2025-02-07 10:19:05 +0100

    fix intersection not symmetric

--- src/main/java/de/uni_passau/fim/se2/litterbox/analytics/pqgram/PQGramProfile.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/analytics/pqgram/PQGramProfile.java
@@ -20,6 +20,7 @@ package de.uni_passau.fim.se2.litterbox.analytics.pqgram;
 
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 import de.uni_passau.fim.se2.litterbox.ast.model.ASTNode;
 
 import java.util.ArrayList;
@@ -67,8 +68,7 @@ public class PQGramProfile {
         if (tuples.isEmpty() && other.size() == 0) {
             return 0;
         }
-        Multiset<LabelTuple> intersection = HashMultiset.create(tuples);
-        intersection.retainAll(other.getTuples());
+        Multiset<LabelTuple> intersection = Multisets.intersection(tuples, other.getTuples());
         double division = (double) intersection.size() / (size() + other.size());
         return 1 - (2 * division);
     }
