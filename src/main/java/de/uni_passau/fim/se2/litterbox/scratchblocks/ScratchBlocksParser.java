5ed263f1e01968eab741030293f9a0370e7fd3ac
Benedikt Fein <fein@fim.uni-passau.de>
2025-05-06 13:30:43 +0200

    scratchblock(refactor): split parser and project merger

--- src/main/java/de/uni_passau/fim/se2/litterbox/scratchblocks/ScratchBlocksParser.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/scratchblocks/ScratchBlocksParser.java
@@ -20,24 +20,9 @@ package de.uni_passau.fim.se2.litterbox.scratchblocks;
 
 import de.uni_passau.fim.se2.litterbox.ast.model.*;
 import de.uni_passau.fim.se2.litterbox.ast.model.event.Never;
-import de.uni_passau.fim.se2.litterbox.ast.model.expression.list.ExpressionList;
-import de.uni_passau.fim.se2.litterbox.ast.model.identifier.Qualified;
-import de.uni_passau.fim.se2.litterbox.ast.model.literals.NumberLiteral;
-import de.uni_passau.fim.se2.litterbox.ast.model.literals.StringLiteral;
-import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.NoBlockMetadata;
-import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinition;
 import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinitionList;
-import de.uni_passau.fim.se2.litterbox.ast.model.statement.common.SetVariableTo;
-import de.uni_passau.fim.se2.litterbox.ast.model.type.NumberType;
-import de.uni_passau.fim.se2.litterbox.ast.model.variable.DataExpr;
-import de.uni_passau.fim.se2.litterbox.ast.model.variable.ScratchList;
-import de.uni_passau.fim.se2.litterbox.ast.model.variable.Variable;
-import de.uni_passau.fim.se2.litterbox.ast.parser.symboltable.ProcedureDefinitionNameMapping;
-import de.uni_passau.fim.se2.litterbox.ast.parser.symboltable.SymbolTable;
 import de.uni_passau.fim.se2.litterbox.ast.util.AstNodeUtil;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.CloneVisitor;
-import de.uni_passau.fim.se2.litterbox.ast.visitor.NodeFilteringVisitor;
-import de.uni_passau.fim.se2.litterbox.ast.visitor.NodeReplacementVisitor;
 import de.uni_passau.fim.se2.litterbox.ast.visitor.ParentVisitor;
 import de.uni_passau.fim.se2.litterbox.generated.ScratchBlocksLexer;
 import org.antlr.v4.runtime.CharStreams;
@@ -70,115 +55,24 @@ import java.util.concurrent.atomic.AtomicBoolean;
  */
 public class ScratchBlocksParser {
 
+    private static final ScratchProjectMerger merger = new ScratchProjectMerger();
+
     // todo: probably similar methods for whole actors and programs?
     public Script parseScript(final String scratchBlocksCode) {
         return parseScript(scratchBlocksCode, new AtomicBoolean(false));
     }
 
     public Program extendProject(Program baseProject, String actorName, String additionalCode) {
+        if (AstNodeUtil.findActorByName(baseProject, actorName) == null) {
+            throw new IllegalArgumentException(
+                    "Unknown actor '" + actorName + "' in project '" + baseProject.getIdent().getName() + "'."
+            );
+        }
+
         CloneVisitor cloneVisitor = new CloneVisitor();
         Program extendedProject = (Program) baseProject.accept(cloneVisitor);
         ActorContent additionalContent = parseActorContent(additionalCode, actorName);
-        addNewFields(additionalContent, extendedProject);
-        addNewProcedureInfo(additionalContent.procedures().getList(), extendedProject.getProcedureMapping(), actorName);
-
-        List<Script> newScripts = new ArrayList<>(additionalContent.scripts().getScriptList());
-        newScripts.addAll(AstNodeUtil.findActorByName(baseProject, actorName).getScripts().getScriptList());
-
-        List<ProcedureDefinition> newProcedures = new ArrayList<>(additionalContent.procedures().getList());
-        newProcedures.addAll(
-                AstNodeUtil.findActorByName(baseProject, actorName).getProcedureDefinitionList().getList()
-        );
-
-        final NodeReplacementVisitor scriptsReplacementVisitor = new NodeReplacementVisitor(
-                AstNodeUtil.findActorByName(extendedProject, actorName).getScripts(),
-                new ScriptList(List.copyOf(newScripts))
-        );
-
-        Program newExtendedProject = (Program) extendedProject.accept(scriptsReplacementVisitor);
-
-        final NodeReplacementVisitor procedureReplacementVisitor = new NodeReplacementVisitor(
-                AstNodeUtil.findActorByName(newExtendedProject, actorName).getProcedureDefinitionList(),
-                new ProcedureDefinitionList(List.copyOf(newProcedures))
-        );
-
-        return (Program) newExtendedProject.accept(procedureReplacementVisitor);
-    }
-
-    private void addNewProcedureInfo(
-            List<ProcedureDefinition> procedures, ProcedureDefinitionNameMapping procedureMapping, String actorName
-    ) {
-        for (ProcedureDefinition procedure : procedures) {
-            procedureMapping.addProcedure(
-                    procedure.getIdent(),
-                    actorName,
-                    procedure.getIdent().getName(),
-                    procedure.getParameterDefinitionList()
-            );
-        }
-    }
-
-    private void addNewFields(ActorContent additionalContent, Program extendedProject) {
-        addQualifiedDataExpressions(additionalContent, extendedProject);
-        addMessages(additionalContent, extendedProject);
-    }
-
-    private void addMessages(ActorContent additionalContent, Program extendedProject) {
-        List<Message> messages = new ArrayList<>();
-        messages.addAll(NodeFilteringVisitor.getBlocks(additionalContent.scripts(), Message.class));
-        messages.addAll(NodeFilteringVisitor.getBlocks(additionalContent.procedures(), Message.class));
-
-        SymbolTable symbolTable = extendedProject.getSymbolTable();
-
-        for (Message message : messages) {
-            if (message.getMessage() instanceof StringLiteral text
-                    && symbolTable.getMessage(text.getText()).isEmpty()) {
-                symbolTable.addMessage(text.getText(), message, true, "Stage", CloneVisitor.generateUID());
-            }
-        }
-    }
-
-    private void addQualifiedDataExpressions(ActorContent additionalContent, Program extendedProject) {
-        List<Qualified> qualifieds = new ArrayList<>();
-        qualifieds.addAll(NodeFilteringVisitor.getBlocks(additionalContent.scripts(), Qualified.class));
-        qualifieds.addAll(NodeFilteringVisitor.getBlocks(additionalContent.procedures(), Qualified.class));
-
-        SymbolTable symbolTable = extendedProject.getSymbolTable();
-        for (Qualified qualified : qualifieds) {
-            String actorInQualified = qualified.getFirst().getName();
-            DataExpr data = qualified.getSecond();
-            if (data instanceof Variable variable) {
-                String varName = variable.getName().getName();
-                if (symbolTable.getVariableIdentifierFromActorAndName(actorInQualified, varName) == null
-                        && symbolTable.getVariableIdentifierFromActorAndName("Stage", varName) == null
-                ) {
-                    String uid = CloneVisitor.generateUID();
-                    symbolTable.addVariable(uid, varName, new NumberType(), false, actorInQualified);
-                    ActorDefinition actor = AstNodeUtil.findActorByName(extendedProject, actorInQualified);
-                    actor.getSetStmtList().getStmts().add(
-                            new SetVariableTo(qualified, new NumberLiteral(0), new NoBlockMetadata())
-                    );
-                }
-            }
-            if (data instanceof ScratchList variable) {
-                String varName = variable.getName().getName();
-                if (symbolTable.getListIdentifierFromActorAndName(actorInQualified, varName) == null
-                        && symbolTable.getListIdentifierFromActorAndName("Stage", varName) == null
-                ) {
-                    String uid = CloneVisitor.generateUID();
-                    symbolTable.addExpressionListInfo(
-                            uid, varName, new ExpressionList(new ArrayList<>()), false, actorInQualified
-                    );
-                    ActorDefinition actor = AstNodeUtil.findActorByName(extendedProject, actorInQualified);
-                    actor.getSetStmtList().getStmts().add(
-                            new SetVariableTo(qualified, new ExpressionList(new ArrayList<>()), new NoBlockMetadata())
-                    );
-                }
-            }
-        }
-    }
-
-    public record ActorContent(ScriptList scripts, ProcedureDefinitionList procedures) {
+        return merger.updateProject(extendedProject, actorName, additionalContent);
     }
 
     public ActorContent parseActorContent(final String scratchBlocksCode) {
---
+++ src/main/java/de/uni_passau/fim/se2/litterbox/scratchblocks/ScratchProjectMerger.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (C) 2019-2024 LitterBox contributors
+ *
+ * This file is part of LitterBox.
+ *
+ * LitterBox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * LitterBox is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LitterBox. If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.uni_passau.fim.se2.litterbox.scratchblocks;
+
+import de.uni_passau.fim.se2.litterbox.ast.model.*;
+import de.uni_passau.fim.se2.litterbox.ast.model.expression.list.ExpressionList;
+import de.uni_passau.fim.se2.litterbox.ast.model.identifier.Qualified;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.NumberLiteral;
+import de.uni_passau.fim.se2.litterbox.ast.model.literals.StringLiteral;
+import de.uni_passau.fim.se2.litterbox.ast.model.metadata.block.NoBlockMetadata;
+import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinition;
+import de.uni_passau.fim.se2.litterbox.ast.model.procedure.ProcedureDefinitionList;
+import de.uni_passau.fim.se2.litterbox.ast.model.statement.common.SetVariableTo;
+import de.uni_passau.fim.se2.litterbox.ast.model.type.NumberType;
+import de.uni_passau.fim.se2.litterbox.ast.model.variable.DataExpr;
+import de.uni_passau.fim.se2.litterbox.ast.model.variable.ScratchList;
+import de.uni_passau.fim.se2.litterbox.ast.model.variable.Variable;
+import de.uni_passau.fim.se2.litterbox.ast.parser.symboltable.ProcedureDefinitionNameMapping;
+import de.uni_passau.fim.se2.litterbox.ast.parser.symboltable.SymbolTable;
+import de.uni_passau.fim.se2.litterbox.ast.util.AstNodeUtil;
+import de.uni_passau.fim.se2.litterbox.ast.visitor.CloneVisitor;
+import de.uni_passau.fim.se2.litterbox.ast.visitor.NodeFilteringVisitor;
+import de.uni_passau.fim.se2.litterbox.ast.visitor.NodeReplacementVisitor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScratchProjectMerger {
+
+    public Program updateProject(Program baseProject, String actorName, ActorContent actorContent) {
+        addNewFields(actorContent, baseProject);
+        addNewProcedureInfo(actorContent.procedures().getList(), baseProject.getProcedureMapping(), actorName);
+
+        List<Script> newScripts = new ArrayList<>(actorContent.scripts().getScriptList());
+        newScripts.addAll(AstNodeUtil.findActorByName(baseProject, actorName).getScripts().getScriptList());
+
+        List<ProcedureDefinition> newProcedures = new ArrayList<>(actorContent.procedures().getList());
+        newProcedures.addAll(
+                AstNodeUtil.findActorByName(baseProject, actorName).getProcedureDefinitionList().getList()
+        );
+
+        final NodeReplacementVisitor scriptsReplacementVisitor = new NodeReplacementVisitor(
+                AstNodeUtil.findActorByName(baseProject, actorName).getScripts(),
+                new ScriptList(List.copyOf(newScripts))
+        );
+
+        Program newExtendedProject = (Program) baseProject.accept(scriptsReplacementVisitor);
+
+        final NodeReplacementVisitor procedureReplacementVisitor = new NodeReplacementVisitor(
+                AstNodeUtil.findActorByName(newExtendedProject, actorName).getProcedureDefinitionList(),
+                new ProcedureDefinitionList(List.copyOf(newProcedures))
+        );
+
+        return (Program) newExtendedProject.accept(procedureReplacementVisitor);
+    }
+
+    public Program updateProject(Program baseProject, String actorName, ScriptEntity newScript) {
+        throw new UnsupportedOperationException("todo");
+    }
+
+    void addNewProcedureInfo(
+            List<ProcedureDefinition> procedures, ProcedureDefinitionNameMapping procedureMapping, String actorName
+    ) {
+        for (ProcedureDefinition procedure : procedures) {
+            procedureMapping.addProcedure(
+                    procedure.getIdent(),
+                    actorName,
+                    procedure.getIdent().getName(),
+                    procedure.getParameterDefinitionList()
+            );
+        }
+    }
+
+    void addNewFields(ActorContent additionalContent, Program extendedProject) {
+        addQualifiedDataExpressions(additionalContent, extendedProject);
+        addMessages(additionalContent, extendedProject);
+    }
+
+    void addMessages(ActorContent additionalContent, Program extendedProject) {
+        List<Message> messages = new ArrayList<>();
+        messages.addAll(NodeFilteringVisitor.getBlocks(additionalContent.scripts(), Message.class));
+        messages.addAll(NodeFilteringVisitor.getBlocks(additionalContent.procedures(), Message.class));
+
+        SymbolTable symbolTable = extendedProject.getSymbolTable();
+
+        for (Message message : messages) {
+            if (message.getMessage() instanceof StringLiteral text
+                    && symbolTable.getMessage(text.getText()).isEmpty()) {
+                symbolTable.addMessage(text.getText(), message, true, "Stage", CloneVisitor.generateUID());
+            }
+        }
+    }
+
+    void addQualifiedDataExpressions(ActorContent additionalContent, Program extendedProject) {
+        List<Qualified> qualifieds = new ArrayList<>();
+        qualifieds.addAll(NodeFilteringVisitor.getBlocks(additionalContent.scripts(), Qualified.class));
+        qualifieds.addAll(NodeFilteringVisitor.getBlocks(additionalContent.procedures(), Qualified.class));
+
+        SymbolTable symbolTable = extendedProject.getSymbolTable();
+        for (Qualified qualified : qualifieds) {
+            String actorInQualified = qualified.getFirst().getName();
+            DataExpr data = qualified.getSecond();
+            if (data instanceof Variable variable) {
+                String varName = variable.getName().getName();
+                if (symbolTable.getVariableIdentifierFromActorAndName(actorInQualified, varName) == null
+                        && symbolTable.getVariableIdentifierFromActorAndName("Stage", varName) == null
+                ) {
+                    String uid = CloneVisitor.generateUID();
+                    symbolTable.addVariable(uid, varName, new NumberType(), false, actorInQualified);
+                    ActorDefinition actor = AstNodeUtil.findActorByName(extendedProject, actorInQualified);
+                    actor.getSetStmtList().getStmts().add(
+                            new SetVariableTo(qualified, new NumberLiteral(0), new NoBlockMetadata())
+                    );
+                }
+            }
+            if (data instanceof ScratchList variable) {
+                String varName = variable.getName().getName();
+                if (symbolTable.getListIdentifierFromActorAndName(actorInQualified, varName) == null
+                        && symbolTable.getListIdentifierFromActorAndName("Stage", varName) == null
+                ) {
+                    String uid = CloneVisitor.generateUID();
+                    symbolTable.addExpressionListInfo(
+                            uid, varName, new ExpressionList(new ArrayList<>()), false, actorInQualified
+                    );
+                    ActorDefinition actor = AstNodeUtil.findActorByName(extendedProject, actorInQualified);
+                    actor.getSetStmtList().getStmts().add(
+                            new SetVariableTo(qualified, new ExpressionList(new ArrayList<>()), new NoBlockMetadata())
+                    );
+                }
+            }
+        }
+    }
+}
