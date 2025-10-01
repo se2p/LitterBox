a5292d503d7bd17e5abede1b600b3217b8eeee78
Benedikt Fein <fein@fim.uni-passau.de>
2025-10-01 17:20:01 +0200

    update version to 1.10

--- src/main/java/de/uni_passau/fim/se2/litterbox/Main.java
+++ src/main/java/de/uni_passau/fim/se2/litterbox/Main.java
@@ -34,7 +34,7 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(
         name = "LitterBox",
         mixinStandardHelpOptions = true,
-        version = "LitterBox 1.10-SNAPSHOT",
+        version = "LitterBox 1.10",
         subcommands = {
                 // general commands
                 Main.CheckProgramsSubcommand.class,
